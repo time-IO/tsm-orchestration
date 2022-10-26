8773387f78180579c083d48b9df67a6d03a5ec80
Luca Johannes Nendel <luca-johannes.nendel@ufz.de>
2022-10-26 11:32:36 +0200

    fix cicd test

--- .gitlab/ci/databasetest.sh
+++ .gitlab/ci/databasetest.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-exists="$(psql 'postgresql://postgres:postgres@docker:5432' -tAc "SELECT 1 FROM pg_roles WHERE rolname='yfirstproject_6185a5b8462711ec910a125e5a40a845'")"
+exists="$(psql 'postgresql://postgres:postgres@docker:5432' -tAc "SELECT 1 FROM pg_roles WHERE rolname='myfirstproject_6185a5b8462711ec910a125e5a40a845'")"
 if [ "$exists" != 1 ]; then
   exit 1
 else
