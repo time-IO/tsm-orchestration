9cd41e43e94be1112f3dab2a49ce051b844aa9d4
Luca Johannes Nendel <luca-johannes.nendel@ufz.de>
2022-10-26 10:48:33 +0200

    test fix

--- .gitlab/ci/databasetest.sh
+++ .gitlab/ci/databasetest.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-exists="$(psql postgresql://postgres:postgres@docker:5432 -tAc SELECT 1 FROM pg_roles WHERE rolname='myfirstproject_6185a5b8462711ec910a125e5a40a845')"
-if [ "$exists" != 0 ]; then
+exists="$(psql 'postgresql://postgres:postgres@localhost:5432' -tAc 'SELECT 1 FROM pg_roles WHERE rolname=myfirstproject_6185a5b8462711ec910a125e5a40a845')"
+if [ "$running" != 0 ]; then
   exit 1
 else
   echo "Database is working correctly"
