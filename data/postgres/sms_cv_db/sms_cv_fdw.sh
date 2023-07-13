edc8a13c8c0520ccdb5531c099be73d7ab1f6fca
granseef <florian.gransee@ufz.de>
2023-07-13 12:31:30 +0200

    add env variables

--- data/postgres/sms_cv_db/sms_cv_fdw.sh
+++ data/postgres/sms_cv_db/sms_cv_fdw.sh
@@ -2,7 +2,6 @@
 set -e
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-    CREATE extension postgres_fdw;
     CREATE SERVER sms_cv_db
       FOREIGN DATA WRAPPER postgres_fdw
       OPTIONS (host '$CV_DB_HOST', dbname '$CV_DB_DB', port '$CV_DB_PORT');
