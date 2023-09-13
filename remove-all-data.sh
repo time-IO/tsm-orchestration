4a51855d0f41aa52fbf97060f2972edf6d4aec99
Bert Palm <bert.palm@ufz.de>
2023-09-13 14:02:35 +0200

    make minio use a single drive

--- remove-all-data.sh
+++ remove-all-data.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env bash
 
 rm -rf data/minio/vol0/* data/minio/vol0/.minio.sys data/minio/vol0/.writable-check-*
-rm -rf data/minio/vol1/* data/minio/vol1/.minio.sys data/minio/vol1/.writable-check-*
-rm -rf data/minio/vol2/* data/minio/vol2/.minio.sys data/minio/vol2/.writable-check-*
-rm -rf data/minio/vol3/* data/minio/vol3/.minio.sys data/minio/vol3/.writable-check-*
 rm -rf data/minio/certs/*
 rm -rf data/postgres/data/*
 touch data/postgres/data/.gitkeep
