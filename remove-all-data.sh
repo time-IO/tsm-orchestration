50b7ec016d6b29877b73afee5b05f64086b5d601
Joost Hemmen <joost.hemmen@ufz.de>
2023-10-18 14:26:16 +0200

    remove force flag from rm to allow prompts, add tomcat directory

--- remove-all-data.sh
+++ remove-all-data.sh
@@ -1,12 +1,9 @@
 #!/usr/bin/env bash
 
-rm -rf data/minio/vol0/* data/minio/vol0/.minio.sys data/minio/vol0/.writable-check-*
-rm -rf data/minio/vol1/* data/minio/vol1/.minio.sys data/minio/vol1/.writable-check-*
-rm -rf data/minio/vol2/* data/minio/vol2/.minio.sys data/minio/vol2/.writable-check-*
-rm -rf data/minio/vol3/* data/minio/vol3/.minio.sys data/minio/vol3/.writable-check-*
-rm -rf data/minio/certs/*
-rm -rf data/postgres/data/*
-touch data/postgres/data/.gitkeep
+rm -r data/minio/vol0/* data/minio/vol0/.minio.sys
+rm -r data/minio/certs/*
+rm -r data/postgres/data/*
 rm data/mosquitto/auth/mosquitto.acl
 rm data/mosquitto/auth/mosquitto.passwd
-rm -rf data/grafana/*
+rm -r data/grafana/*
+rm -r data/tomcat/context/*
