88e9ba5f78961ecfff08c88915ef590cb3c0f1f4
Joost Hemmen <joost.hemmen@ufz.de>
2023-10-26 14:21:36 +0200

    Update file proxytest.sh

--- .gitlab/ci/proxytest.sh
+++ .gitlab/ci/proxytest.sh
@@ -6,15 +6,15 @@ GATEWAY_IP=$(docker inspect tsm-orchestration-proxy-1 | jq -r .[].NetworkSetting
 curl --version
 
 echo "running proxytest.sh"
-echo "checking http://${PROXY_IP}:80"
-curl ${PROXY_IP}:80 && echo yes || echo no
-echo ""
-echo "checking http://${PROXY_IP}"
-curl ${PROXY_IP} && echo yes || echo no
-echo ""
-echo "checking http://${GATEWAY_IP}:80"
-curl ${GATEWAY_IP}:80 && echo yes || echo no
-echo ""
-echo "checking http://${GATEWAY_IP}"
-curl ${GATEWAY_IP} && echo yes || echo no
+echo "checking http://0.0.0.0:80"
+curl 0.0.0.0:80 && echo yes || echo no
+#echo ""
+echo "checking http://0.0.0.0"
+curl 0.0.0.0 && echo yes || echo no
+#echo ""
+#echo "checking http://${GATEWAY_IP}:80"
+#curl ${GATEWAY_IP}:80 && echo yes || echo no
+#echo ""
+#echo "checking http://${GATEWAY_IP}"
+#curl ${GATEWAY_IP} && echo yes || echo no
 
