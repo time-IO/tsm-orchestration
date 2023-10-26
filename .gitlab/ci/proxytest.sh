c3f5acdada708c637562a851bf84316b36b8e81c
Joost Hemmen <joost.hemmen@ufz.de>
2023-10-26 13:16:10 +0200

    Update file proxytest.sh

--- .gitlab/ci/proxytest.sh
+++ .gitlab/ci/proxytest.sh
@@ -1,27 +1,18 @@
 #!/bin/bash
 PROXY_IP=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' tsm-orchestration-proxy-1)
-echo $PROXY_IP
-echo "checking http://${POXY_IP}:80"
-curl -I http://$PROXY_IP:80 && echo yes || echo no
-echo "checking http://${POXY_IP}"
-curl -I http://$PROXY_IP && echo yes || echo no
-echo "checking http://localhost"
-curl -I http://localhost && echo yes || echo no
-echo "checking http://127.0.0.1"
-curl -I http://127.0.0.1 && echo yes || echo no
-echo "checking http://docker"
-curl -I http://docker && echo yes || echo no
-cho "checking http://docker."
-curl -I http://docker. && echo yes || echo no
-echo "checking http://docker:80"
-curl -I http://docker:80 && echo yes || echo no
-cho "checking http://docker.:80"
-curl -I http://docker.:80 && echo yes || echo no
-echo "checking http://proxy"
-curl -I http://proxy && echo yes || echo no
-echo "checking http://proxy."
-curl -I http://proxy. && echo yes || echo no
-echo "checking http://proxy:80"
-curl -I http://proxy:80 && echo yes || echo no
-echo "checking http://proxy.:80"
-curl -I http://proxy.:80 && echo yes || echo no
+PROXY_IP2=$(docker inspect tsm-orchestration-proxy-1 | jq -r .[].NetworkSettings.Networks.\"tsm-orchestration_default\".IPAddress)
+GATEWAY_IP=$(docker inspect tsm-orchestration-proxy-1 | jq -r .[].NetworkSettings.Networks.\"tsm-orchestration_default\".Gateway)
+echo "running proxytest.sh"
+echo "checking http://${PROXY_IP}:80"
+curl http://${PROXY_IP}:80 && echo yes || echo no
+echo "checking http://${PROXY_IP}"
+curl http://${PROXY_IP} && echo yes || echo no
+echo "checking http://${PROXY_IP2}:80"
+curl http://${PROXY_IP2}:80 && echo yes || echo no
+echo "checking http://${PROXY_IP2}"
+curl http://${PROXY_IP2} && echo yes || echo no
+echo "checking http://${GATEWAY_IP}:80"
+curl http://${GATEWAY_IP}:80 && echo yes || echo no
+echo "checking http://${GATEWAY_IP}"
+curl http://${GATEWAY_IP} && echo yes || echo no
+
