3176f344951d74221cdd559a6f083e7b36e81d73
Joost Hemmen <joost.hemmen@ufz.de>
2023-10-26 13:45:57 +0200

    Update file proxytest.sh

--- .gitlab/ci/proxytest.sh
+++ .gitlab/ci/proxytest.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
-PROXY_IP=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' tsm-orchestration-proxy-1)
-PROXY_IP2=$(docker inspect tsm-orchestration-proxy-1 | jq -r .[].NetworkSettings.Networks.\"tsm-orchestration_default\".IPAddress)
+
+PROXY_IP=$(docker inspect tsm-orchestration-proxy-1 | jq -r .[].NetworkSettings.Networks.\"tsm-orchestration_default\".IPAddress)
 GATEWAY_IP=$(docker inspect tsm-orchestration-proxy-1 | jq -r .[].NetworkSettings.Networks.\"tsm-orchestration_default\".Gateway)
+
+curl --version
+
 echo "running proxytest.sh"
 echo "checking http://${PROXY_IP}:80"
-curl http://${PROXY_IP}:80 && echo yes || echo no
+curl ${PROXY_IP}:80 && echo yes || echo no
+echo ""
 echo "checking http://${PROXY_IP}"
-curl http://${PROXY_IP} && echo yes || echo no
-echo "checking http://${PROXY_IP2}:80"
-curl http://${PROXY_IP2}:80 && echo yes || echo no
-echo "checking http://${PROXY_IP2}"
-curl http://${PROXY_IP2} && echo yes || echo no
+curl ${PROXY_IP} && echo yes || echo no
+echo ""
 echo "checking http://${GATEWAY_IP}:80"
-curl http://${GATEWAY_IP}:80 && echo yes || echo no
+curl ${GATEWAY_IP}:80 && echo yes || echo no
+echo ""
 echo "checking http://${GATEWAY_IP}"
-curl http://${GATEWAY_IP} && echo yes || echo no
+curl ${GATEWAY_IP} && echo yes || echo no
 
