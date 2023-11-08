e8c639f63790338627de5daa40dcfd0d51a2cfd5
joost.hemmen1 <joost.hemmen@ufz.de>
2023-11-08 21:28:01 +0100

    clean up and fix dockertest.sh

--- .gitlab/ci/dockertest.sh
+++ .gitlab/ci/dockertest.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-running="$(docker compose ps --services --filter "status=running")"
-services="$(docker compose ps --services)"
+running="$(docker compose ps --services --filter status=running | sort)"
+services="$(docker compose config --services | grep -v init | sort)"
 if [ "$running" != "$services" ]; then
     echo "Following services are not running:"
     # Bash specific
     comm -13 <(sort <<<"$running") <(sort <<<"$services")
+    exit 1
 else
     echo "All services are running"
+    exit 0
 fi
