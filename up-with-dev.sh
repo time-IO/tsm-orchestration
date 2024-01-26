196574496bd905291711af165cb89bb8ca9f22c6
Tobias Kuhnert <tobias.kiuhnert@ufz.de>
2024-01-26 10:50:18 +0100

    added script to start and stop the services for different configurations

---
+++ up-with-dev.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+DIR_SCRIPT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+docker compose -f "${DIR_SCRIPT}/docker-compose.yml" -f "${DIR_SCRIPT}/docker-compose-base.yml" -f "${DIR_SCRIPT}/docker-compose-worker.yml" -f "${DIR_SCRIPT}/docker-compose-dev.yml"  up -d
