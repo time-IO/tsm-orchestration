2ab8acce92388b8a61ce274094740f324a737c05
Bert Palm <bert.palm@ufz.de>
2024-06-05 11:51:21 +0200

    add NULL as default

--- dispatcher/s3_to_db.sh
+++ dispatcher/s3_to_db.sh
@@ -17,8 +17,8 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-E
         "thing_uuid"        uuid         NOT NULL,
         "thing_name"        varchar(256) NOT NULL,
         "db_url"            varchar(256) NOT NULL,
-        "filename_pattern"  varchar(256),
-        "parser"            varchar(256)
+        "filename_pattern"  varchar(256) NULL,
+        "parser"            varchar(256) NULL
     );
 
     GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA $S3MAP_POSTGRES_USER TO $S3MAP_POSTGRES_USER;
