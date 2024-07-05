095d444cbb727a45dc8b1786db488bd7ebc65a9d
granseef <florian.gransee@ufz.de>
2024-07-05 11:45:32 +0200

    fix constraint

--- migrations/init_schema_thing_mapping.sh
+++ migrations/init_schema_thing_mapping.sh
@@ -6,6 +6,6 @@ psql -v ON_ERROR_STOP=1 -U "$POSTGRES_USER" <<-EOSQL
     CREATE TABLE IF NOT EXISTS public."schema_thing_mapping"(
 	  "schema_name"		  VARCHAR(100) NOT NULL,
     "thing_uuid"	UUID NOT NULL,
-	  UNIQUE("schema", "thing_uuid"));
+	  UNIQUE("schema_name", "thing_uuid"));
 EOSQL
 echo 'schema-thing-mapping table created'
