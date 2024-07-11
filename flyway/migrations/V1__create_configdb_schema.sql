65ac25892d649ba14f7fce7463e83a23c0014cb9
granseef <florian.gransee@ufz.de>
2024-07-11 11:34:05 +0200

    add baselineVersion handling

--- flyway/migrations/V1__create_configdb_schema.sql
+++
@@ -1,7 +0,0 @@
-CREATE ROLE ${configdb_user} WITH LOGIN PASSWORD '${configdb_password}';
-GRANT ${configdb_user} TO ${flyway:user};
-CREATE SCHEMA IF NOT EXISTS ${configdb_user} AUTHORIZATION ${configdb_user};
-GRANT CONNECT ON DATABASE ${flyway:database} TO ${configdb_user};
-ALTER ROLE ${configdb_user} SET search_path TO ${configdb_user};
-GRANT USAGE ON SCHEMA ${configdb_user} TO ${configdb_user};
-GRANT ALL ON SCHEMA ${configdb_user} TO ${configdb_user};
