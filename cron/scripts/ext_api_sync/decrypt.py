2ac953bf312e35be2dd2640ed1be5da73c47bf55
granseef <florian.gransee@ufz.de>
2025-01-31 11:02:05 +0100

    small adjustment

--- cron/scripts/ext_api_sync/decrypt.py
+++ cron/scripts/ext_api_sync/decrypt.py
@@ -2,6 +2,7 @@ import os
 
 from cryptography.fernet import Fernet
 
-def decrypt(token: str):
+
+def decrypt(token: str) -> str:
     key = os.environ["FERNET_ENCRYPTION_SECRET"]
     return Fernet(key).decrypt(token).decode()
