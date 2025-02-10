6f2a208e9c3d05d2263515909f093699b9d2a348
Joost Hemmen <joost.hemmen@ufz.de>
2025-02-10 17:05:41 +0100

    Update file deploy.sh

--- deployment/tsm.intranet.ufz.de/deploy.sh
+++ deployment/tsm.intranet.ufz.de/deploy.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env sh
 cd "$(dirname "$0")/../.."
-
+# todo: checkout origin main .env.example into temp.env
+# run check using temp.env 
+./compare_dotenv_files.py .env.example .env
 git pull
 rm remove-all-data.sh
 sudo docker create --build
