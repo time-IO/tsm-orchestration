6507f189def35e45b75fa19516d66acbad1d6c94
granseef <florian.gransee@ufz.de>
2025-02-21 11:22:27 +0100

    update crontab_setup

--- src/setup_crontab.py
+++ src/setup_crontab.py
@@ -57,28 +57,28 @@ class CreateThingInCrontabHandler(AbstractHandler):
         info = ""
         comment = cls.mk_comment(thing)
         uuid = thing.uuid
-        if thing.external_sftp is not None:
-            interval = int(thing.external_sftp.sync_interval)
+        if thing.ext_sftp is not None:
+            interval = int(thing.ext_sftp.sync_interval)
             schedule = cls.get_schedule(interval)
             script = "/scripts/sync_sftp.py"
-            keyfile = thing.external_sftp.private_key_path
+            keyfile = thing.ext_sftp.ssh_priv_key
             command = f"{script} {uuid} {keyfile} > $STDOUT 2> $STDERR"
-            job.enable(enabled=thing.external_sftp.enabled)
+            job.enable(enabled=thing.ext_sftp.sync_enabled)
             job.set_comment(comment, pre_comment=True)
             job.setall(schedule)
             job.set_command(command)
-            info = f"sFTP {thing.external_sftp.uri} @ {interval}s"
-        if thing.external_api is not None:
-            interval = int(thing.external_api.sync_interval)
+            info = f"sFTP {thing.ext_sftp.uri} @ {interval}s"
+        if thing.ext_api is not None:
+            interval = int(thing.ext_api.sync_interval)
             schedule = cls.get_schedule(interval)
-            script = f"/scripts/sync_{thing.external_api.api_type_name}_api.py"
+            script = f"/scripts/sync_{thing.ext_api.api_type_name}_api.py"
             target_uri = thing.database.url
-            command = f"""{script} {uuid} "{thing.external_api.settings}" {target_uri} > $STDOUT 2> $STDERR"""
-            job.enable(enabled=thing.external_api.enabled)
+            command = f"""{script} {uuid} "{thing.ext_api.settings}" {target_uri} > $STDOUT 2> $STDERR"""
+            job.enable(enabled=thing.ext_api.sync_enabled)
             job.set_comment(comment, pre_comment=True)
             job.setall(schedule)
             job.set_command(command)
-            info = f"{thing.external_api.api_type_name}-API @ {interval}s"
+            info = f"{thing.ext_api.api_type_name}-API @ {interval}s"
         return info
 
     # alias
