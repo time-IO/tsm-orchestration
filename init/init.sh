7bec059a11daa606f11bbd5db8bfca253d0aaeda
Joost Hemmen <joost.hemmen@ufz.de>
2025-03-11 17:26:47 +0100

    first draft docker volumes

--- init/init.sh
+++ init/init.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env sh
 
 # Create a SSH private key file when it is not already present for object storage (minio) sftp service
-ls -lah /tmp/certs/id_ed25519 2>/dev/null || ssh-keygen -t ed25519 -f /tmp/certs/id_ed25519 -N ""
+ls -lah /tmp/minio/certs/id_ed25519 2>/dev/null || ssh-keygen -t ed25519 -f /tmp/minio/certs/id_ed25519 -N ""
 
 # Create TLS key and cert for object storage (minio) FTP service when not already present or expired
-ls -lah /tmp/certs/minio-ftp.key /tmp/certs/minio-ftp.crt 2>/dev/null \
-  && openssl x509 -enddate -noout -in /tmp/certs/minio-ftp.crt -checkend 604800 \
+ls -lah /tmp/minio/certs/minio-ftp.key /tmp/minio/certs/minio-ftp.crt 2>/dev/null \
+  && openssl x509 -enddate -noout -in /tmp/minio/certs/minio-ftp.crt -checkend 604800 \
   || openssl req -new -newkey ed25519 -days 90 -nodes -x509 \
-    -keyout /tmp/certs/minio-ftp.key \
-    -out /tmp/certs/minio-ftp.crt \
+    -keyout /tmp/minio/certs/minio-ftp.key \
+    -out /tmp/minio/certs/minio-ftp.crt \
     -subj "/C=DE/O=Helmholtz-Zentrum für Umweltforschung GmbH - UFZ/OU=RDM/CN=ZID TSM Development CA" \
     -addext "subjectAltName = DNS:localhost" \
     -addext "basicConstraints=critical,CA:FALSE"
