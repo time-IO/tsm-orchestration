73548f8080df16a385308b3e834f672b9330a672
joost.hemmen1 <joost.hemmen@ufz.de>
2025-03-17 23:24:50 +0100

    fix cert paths

--- init/init.sh
+++ init/init.sh
@@ -6,18 +6,21 @@ mkdir -p /tmp/minio/vol0
 # 1. Always copy SSH key from host if it exists
 #    - If not, check if it exists in certs
 #    - If not, generate a new SSH key
-if [ -f "/tmp/minio/hostcerts/id_ed25519" ]; then
-    cp /tmp/minio/hostcerts/id_ed25519 /tmp/minio/certs/id_ed25519
+if [ -f "/tmp/hostcerts/id_ed25519" ]; then
+    echo "Copying SSH key from host."
+    cp /tmp/hostcerts/id_ed25519 /tmp/minio/certs/id_ed25519
 elif [ ! -f "/tmp/minio/certs/id_ed25519" ]; then
+    echo "No SSH key found, generating new SSH key."
     ssh-keygen -t ed25519 -f /tmp/minio/certs/id_ed25519 -N ""
 fi
 
 # 2. Always copy FTP cert and key from host if they exist
 #    - If not, check if they exist in certs and are valid
 #    - If not, generate new self-signed certificates
-if [ -f "/tmp/minio/hostcerts/minio-ftp.crt" ] && [ -f "/tmp/minio/hostcerts/minio-ftp.key" ]; then
-    cp /tmp/minio/hostcerts/minio-ftp.crt /tmp/minio/certs/minio-ftp.crt
-    cp /tmp/minio/hostcerts/minio-ftp.key /tmp/minio/certs/minio-ftp.key
+if [ -f "/tmp/hostcerts/minio-ftp.crt" ] && [ -f "/tmp/hostcerts/minio-ftp.key" ]; then
+    echo "Copying FTP certificate and key from host."
+    cp /tmp/hostcerts/minio-ftp.crt /tmp/minio/certs/minio-ftp.crt
+    cp /tmp/hostcerts/minio-ftp.key /tmp/minio/certs/minio-ftp.key
 else
     if [ ! -f "/tmp/minio/certs/minio-ftp.crt" ] || [ ! -f "/tmp/minio/certs/minio-ftp.key" ] || \
        ! openssl x509 -checkend 604800 -noout -in /tmp/minio/certs/minio-ftp.crt; then
