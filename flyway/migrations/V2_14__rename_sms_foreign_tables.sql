afde06b6e7379461c683ec043f28d0ac43763cd6
Tobias Kuhnert <tobias.kiuhnert@ufz.de>
2025-03-20 09:50:31 +0100

    added extra if statement to avoid unnecessary execution

--- flyway/migrations/V2_14__rename_sms_foreign_tables.sql
+++ flyway/migrations/V2_14__rename_sms_foreign_tables.sql
@@ -1,25 +1,37 @@
-DO $$
-DECLARE
-    table_names text[] := ARRAY[
-        'sms_contact',
-        'sms_configuration',
-        'sms_configuration_contact_role',
-        'sms_configuration_dynamic_location_begin_action',
-        'sms_configuration_static_location_begin_action',
-        'sms_device',
-        'sms_device_mount_action',
-        'sms_device_property',
-        'sms_device_contact_role',
-        'sms_datastream_link'
-    ];
-    table_name text;
-BEGIN
-    FOREACH table_name IN ARRAY table_names LOOP
-        IF EXISTS (SELECT 1 FROM information_schema.foreign_tables WHERE foreign_table_name = table_name AND foreign_table_schema = 'public') THEN
-            EXECUTE format('ALTER FOREIGN TABLE public.%s RENAME TO foreign_table_%s;', table_name, table_name);
-            RAISE NOTICE 'Renamed foreign table % ', table_name;
+DO
+$$
+    DECLARE
+        table_names text[] := ARRAY [
+            'sms_contact',
+            'sms_configuration',
+            'sms_configuration_contact_role',
+            'sms_configuration_dynamic_location_begin_action',
+            'sms_configuration_static_location_begin_action',
+            'sms_device',
+            'sms_device_mount_action',
+            'sms_device_property',
+            'sms_device_contact_role',
+            'sms_datastream_link'
+            ];
+        table_name  text;
+    BEGIN
+        IF '${sms_access_type}' = 'db' THEN
+            RAISE NOTICE 'Renaming foreign tables of sms in order to use materialized views instead';
+            FOREACH table_name IN ARRAY table_names
+                LOOP
+                    IF EXISTS (SELECT 1
+                               FROM information_schema.foreign_tables
+                               WHERE foreign_table_name = table_name
+                                 AND foreign_table_schema = 'public') THEN
+                        EXECUTE format('ALTER FOREIGN TABLE public.%s RENAME TO foreign_table_%s;', table_name,
+                                       table_name);
+                        RAISE NOTICE 'Renamed foreign table % ', table_name;
+                    ELSE
+                        RAISE NOTICE 'Foreign table % does not exist, skipping rename', table_name;
+                    END IF;
+                END LOOP;
         ELSE
-            RAISE NOTICE 'Foreign table % does not exist, skipping rename', table_name;
+            RAISE NOTICE 'Skipping renaming of foreign tables for sms because sms_access_type is not db';
         END IF;
-    END LOOP;
-END $$;
+    END
+$$;
--- flyway/migrations/V2_15__materialized_views_for_sms_foreign_tables.sql
+++ flyway/migrations/V2_15__materialized_views_for_sms_foreign_tables.sql
@@ -1,31 +1,44 @@
-DO $$
-DECLARE
-    table_names text[] := ARRAY[
-        'contact',
-        'configuration',
-        'configuration_contact_role',
-        'configuration_dynamic_location_begin_action',
-        'configuration_static_location_begin_action',
-        'device',
-        'device_contact_role',
-        'device_mount_action',
-        'device_property',
-        'datastream_link'
-    ];
-    table_name text;
-    ft_name text;
-    materialized_view_name text;
-BEGIN
-    FOREACH table_name IN ARRAY table_names LOOP
-        ft_name := 'foreign_table_sms_' || table_name;
-        materialized_view_name := 'sms_' || table_name;
+DO
+$$
+    DECLARE
+        table_names            text[] := ARRAY [
+            'contact',
+            'configuration',
+            'configuration_contact_role',
+            'configuration_dynamic_location_begin_action',
+            'configuration_static_location_begin_action',
+            'device',
+            'device_contact_role',
+            'device_mount_action',
+            'device_property',
+            'datastream_link'
+            ];
+        table_name             text;
+        ft_name                text;
+        materialized_view_name text;
+    BEGIN
+        IF '${sms_access_type}' = 'db' THEN
+            RAISE NOTICE 'Creating materialized views for foreign tables of sms';
+            FOREACH table_name IN ARRAY table_names
+                LOOP
+                    ft_name := 'foreign_table_sms_' || table_name;
+                    materialized_view_name := 'sms_' || table_name;
 
-        IF EXISTS (SELECT 1 FROM information_schema.foreign_tables WHERE foreign_table_name = ft_name AND foreign_table_schema = 'public') THEN
-            EXECUTE format('CREATE MATERIALIZED VIEW public.%s AS SELECT * FROM public.%s;', materialized_view_name, ft_name);
-            EXECUTE format('CREATE UNIQUE INDEX %s_pkey ON public.%s (id);', materialized_view_name, materialized_view_name);
-            RAISE NOTICE 'Created materialized view %s and index on primary key', materialized_view_name;
+                    IF EXISTS (SELECT 1
+                               FROM information_schema.foreign_tables
+                               WHERE foreign_table_name = ft_name
+                                 AND foreign_table_schema = 'public') THEN
+                        EXECUTE format('CREATE MATERIALIZED VIEW public.%s AS SELECT * FROM public.%s;',
+                                       materialized_view_name, ft_name);
+                        EXECUTE format('CREATE UNIQUE INDEX %s_pkey ON public.%s (id);', materialized_view_name,
+                                       materialized_view_name);
+                        RAISE NOTICE 'Created materialized view %s and index on primary key', materialized_view_name;
+                    ELSE
+                        RAISE NOTICE 'Foreign table %s does not exist, skipping materialized view creation', ft_name;
+                    END IF;
+                END LOOP;
         ELSE
-            RAISE NOTICE 'Foreign table %s does not exist, skipping materialized view creation', ft_name;
+            RAISE NOTICE 'Skipping creating materialized views for sms because sms_access_type is not db';
         END IF;
-    END LOOP;
-END $$;
+    END
+$$;
