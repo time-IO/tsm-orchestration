b737e9c0d57ea6dd1a00c13439f94b03f38ad294
Tobias Kuhnert <tobias.kiuhnert@ufz.de>
2025-04-03 09:34:51 +0200

    removed own docker file and use configdb-update image instead

--- publish-all-things.sh
+++ publish-all-things.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 DIR_SCRIPT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-docker compose -f "${DIR_SCRIPT}/docker-compose.all-things-publisher.yml" run --rm  all-things-publisher python publish_all_things.py
+docker compose -f "${DIR_SCRIPT}/docker-compose.all-things-publisher.yml" run --rm  all-things-publisher publish_all_things.py
