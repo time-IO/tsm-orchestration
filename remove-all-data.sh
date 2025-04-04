f465b1020cc28481002ba5167d5e82d9fdbfb5f2
joost.hemmen1 <joost.hemmen@ufz.de>
2025-04-04 22:29:46 +0200

    clean up data directories and remove-all-data.sh

--- remove-all-data.sh
+++ remove-all-data.sh
@@ -1,11 +1,9 @@
 #!/usr/bin/env bash
 
-rm -r data/minio/vol0/* data/minio/vol0/.minio.sys
-rm -r data/minio/certs/*
+rm -r data/minio/**/* data/minio/vol0/.minio.sys
 rm -r data/postgres/data/*
-rm data/mosquitto/auth/mosquitto.acl
-rm data/mosquitto/auth/mosquitto.passwd
+rm data/mosquitto/**/*
 rm -r data/grafana/*
 rm -r data/tomcat/context/*
-rm cron/crontab.txt && touch cron/crontab.txt
+rm data/cron/*
 exit 0
