b232854720aeb8493274ada6b54a0fd0f2973ad7
David Schäfer <schaefed@mac-schaefed-363.intranet.ufz.de>
2025-04-08 08:03:24 +0200

    renamed migration file

--- flyway/migrations/V2_11__observation_add_id.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE observation ADD COLUMN id BIGINT;
-CREATE SEQUENCE observation_id_seq;
-UPDATE observation SET id = nextval('observation_test_id_seq');
-ALTER TABLE observation ADD CONSTRAINT pk_observation PRIMARY KEY (id);
