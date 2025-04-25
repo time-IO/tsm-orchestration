d02785c49e9d838b70e775f81e31e30a0b63d213
granseef <florian.gransee@ufz.de>
2025-04-25 09:31:46 +0200

    proxy tests  with pytest

---
+++ tests/test_deployment/test_proxy.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+import os
+import requests
+import pytest
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+ENV_KEYS = [
+    "PROXY_URL",
+    "OBJECT_STORAGE_BROWSER_REDIRECT_URL",
+    "VISUALIZATION_PROXY_URL",
+    "STA_PROXY_URL",
+    "THING_MANAGEMENT_LEGACY_PROXY_URL",
+]
+
+
+def get_env_endpoints():
+    env_endpoints = [os.environ.get(env_var) for env_var in ENV_KEYS]
+    return env_endpoints
+
+
+@pytest.mark.parametrize("endpoint", get_env_endpoints())
+def test_proxy_endpoints(endpoint):
+    response = requests.get(endpoint, timeout=5)
+    response.raise_for_status()
