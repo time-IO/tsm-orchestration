f6f65f3d72a4745475cc2df2ad4d046128e1a8cd
Joost Hemmen <joost.hemmen@ufz.de>
2025-05-09 09:01:38 +0200

    Merge branch '172-remove-thing-management-flyway-migrations-for-now' into 'main'
    Resolve "remove thing management flyway migrations for now"
    
    Closes #172
    
    See merge request ufz-tsm/tsm-orchestration!367

--- flyway/migrations/thing-management/V2_0_4__fix_qaqcs.sql
+++
@@ -1,7 +0,0 @@
--- ===========================================
--- Migration: Allow multiple same functions per QAQC setting
--- ===========================================
-
--- Drop the unique constraint from qaqc_setting_function
-ALTER TABLE "qaqc_setting_function"
-    DROP CONSTRAINT IF EXISTS "qaqc_setting_function_qaqc_setting_id_function_id_key";
