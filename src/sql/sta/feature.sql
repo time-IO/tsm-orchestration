4f499430138d0a6e1596455393cd090d656cd5d5
Bert Palm <bert.palm@ufz.de>
2025-05-15 14:14:48 +0200

    rm obsolete code

--- src/sql/sta/feature.sql
+++ src/sql/sta/feature.sql
@@ -1,27 +1,11 @@
-DO $$
-BEGIN
-    IF EXISTS (
-        SELECT 1
-        FROM information_schema.tables
-        WHERE table_name = 'FEATURES'
-        AND table_schema = '{tsm_schema}'
-        AND table_type = 'BASE TABLE')
-    THEN EXECUTE 'DROP TABLE "FEATURES" CASCADE';
-    ELSIF EXISTS (
-        SELECT 1
-        FROM information_schema.tables
-        WHERE table_name = 'FEATURES'
-        AND table_schema = '{tsm_schema}'
-        AND table_type = 'VIEW'
-        )
-    THEN EXECUTE 'DROP VIEW "FEATURES" CASCADE';
-    END IF;
-END $$;
-CREATE TABLE "FEATURES" (
-  "ID" serial,
-  "NAME" text,
-  "DESCRIPTION" text,
-  "ENCODING_TYPE" text,
-  "FEATURE" jsonb,
-  "PROPERTIES" jsonb
+DROP VIEW IF EXISTS "FEATURES" CASCADE;
+DROP TABLE IF EXISTS "FEATURES" CASCADE;
+CREATE TABLE "FEATURES"
+(
+    "ID"            serial,
+    "NAME"          text,
+    "DESCRIPTION"   text,
+    "ENCODING_TYPE" text,
+    "FEATURE"       jsonb,
+    "PROPERTIES"    jsonb
 )
