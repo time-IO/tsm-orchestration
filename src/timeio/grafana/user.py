09b799f4a6dcd9f7e1e5e5a85ce2079fa7d5a2ac
Bert Palm <bert.palm@ufz.de>
2025-05-20 17:46:22 +0200

    me suggestiones

--- src/timeio/grafana/user.py
+++ src/timeio/grafana/user.py
@@ -1,19 +1,16 @@
 from __future__ import annotations
 
 from typing import TYPE_CHECKING
-from logging import Logger
 
-from timeio.grafana.typing import UserT
-from timeio.grafana.utils import _exists
+from timeio.grafana.utils import _exists, logger
 
 if TYPE_CHECKING:
     from timeio.grafana.api import TimeioGrafanaApi
 
 
 class GrafanaUser:
-    def __init__(self, api: TimeioGrafanaApi, logger: Logger) -> None:
+    def __init__(self, api: TimeioGrafanaApi) -> None:
         self.api = api
-        self.logger = logger
 
     def exists(self, name: str) -> bool:
         return _exists(self.api.users.find_user, name)
