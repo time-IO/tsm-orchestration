fdeb74490de15d16b880461aa42a49c197522840
Joost Hemmen <joost.hemmen@ufz.de>
2025-06-25 14:04:46 +0200

    Edit qaqc.sql - remove whitespace

--- src/timeio/grafana/sql/qaqc.sql
+++ src/timeio/grafana/sql/qaqc.sql
@@ -1,22 +1,22 @@
-            -- Using "result_quality -> -1 ->>" because result_quality is an array of quality objects
-            -- we use "-1" to always select the last one
-            SELECT o.result_time AS "time",
-            1 AS "quality_flag",
-            jsonb_build_object(
-                'annotation', CAST ((result_quality -> -1 ->> 'annotation') AS DECIMAL),
-                'measure', result_quality -> -1 ->> 'properties', 'measure',
-                'user_label', result_quality -> -1 ->> 'properties', 'userLabel'
-            ) AS "qaqc_result"
-            FROM observation o
-            WHERE o.datastream_id = (
-                SELECT dp.ds_id
-                FROM datastream_properties dp
-                WHERE ${{datastream_pos:singlequote}} in (dp.property,dp.position)
-                AND dp.t_uuid::text = '{uuid}'
-            ) AND ${{show_qaqc_flags}} = 'True'
-            AND result_quality IS NOT NULL
-            AND result_quality <> 'null'
-            AND (result_quality -> -1 ->> 'annotation') IS NOT NULL
-            AND (result_quality -> -1 ->> 'annotation') <> '0.0'
-            AND (result_quality -> -1 ->> 'annotation') <> '-inf'
-            ORDER BY o.result_time ASC
+-- Using "result_quality -> -1 ->>" because result_quality is an array of quality objects
+-- we use "-1" to always select the last one
+SELECT o.result_time AS "time",
+1 AS "quality_flag",
+jsonb_build_object(
+    'annotation', CAST ((result_quality -> -1 ->> 'annotation') AS DECIMAL),
+    'measure', result_quality -> -1 ->> 'properties', 'measure',
+    'user_label', result_quality -> -1 ->> 'properties', 'userLabel'
+) AS "qaqc_result"
+FROM observation o
+WHERE o.datastream_id = (
+    SELECT dp.ds_id
+    FROM datastream_properties dp
+    WHERE ${{datastream_pos:singlequote}} in (dp.property,dp.position)
+    AND dp.t_uuid::text = '{uuid}'
+) AND ${{show_qaqc_flags}} = 'True'
+AND result_quality IS NOT NULL
+AND result_quality <> 'null'
+AND (result_quality -> -1 ->> 'annotation') IS NOT NULL
+AND (result_quality -> -1 ->> 'annotation') <> '0.0'
+AND (result_quality -> -1 ->> 'annotation') <> '-inf'
+ORDER BY o.result_time ASC
