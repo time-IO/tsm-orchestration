932259cf7b91ab04de429c46b1ffb98dc53a47cd
Florian Gransee <florian.gransee@ufz.de>
2025-07-10 12:52:00 +0200

    Merge branch '643-add-keycloak-to-flyway' into 'main'
    643 add keycloak to flyway
    
    See merge request ufz-tsm/tsm-orchestration!409

---
+++ flyway/migrations/public/V2_20__keycloak_add_schema_and_role.sql
@@ -0,0 +1,10 @@
+CREATE ROLE ${keycloak_db_user} WITH PASSWORD '${keycloak_db_password}';
+GRANT ${keycloak_db_user} TO ${flyway:user};
+CREATE SCHEMA IF NOT EXISTS ${keycloak_db_user} AUTHORIZATION ${keycloak_db_user};
+SET search_path TO ${keycloak_db_user};
+GRANT CONNECT ON DATABASE ${flyway:database} TO ${keycloak_db_user};
+ALTER ROLE ${keycloak_db_user} SET search_path to ${keycloak_db_user};
+GRANT USAGE ON SCHEMA ${keycloak_db_user} TO ${keycloak_db_user};
+GRANT ALL ON SCHEMA ${keycloak_db_user} TO ${keycloak_db_user};
+GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA ${keycloak_db_user} TO ${keycloak_db_user};
+GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA ${keycloak_db_user} TO ${keycloak_db_user};
