c6ec11704f4e877e2faad7f1d1dc9e881660c3f0
Joost Hemmen <joost.hemmen@ufz.de>
2025-07-24 13:22:37 +0200

    Merge branch '192-prepare-mqtt-monitoring' into 'main'
    Resolve "prepare mqtt monitoring"
    
    Closes #192
    
    See merge request ufz-tsm/tsm-orchestration!416

--- src/mqtt_sync_wrapper.py
+++ src/mqtt_sync_wrapper.py
@@ -124,5 +124,10 @@ def sync_sms(origin: str):
     publish_single(get_envvar("SMS_SYNC_TOPIC"), json.dumps(message))
 
 
+@cli.command()
+def mqtt_monitoring():
+    publish_single(get_envvar("MQTT_MONITORING_TOPIC"), json.dumps({}))
+
+
 if __name__ == "__main__":
     cli()
