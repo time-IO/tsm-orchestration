f734cfb86140cf6934c98c321332618fc3970d6a
Johanna Mardt <johanna.mardt@ufz.de>
2025-08-26 13:44:18 +0200

    aufgeräumt, FoI

--- src/sql/sta/neuFoI.sql
+++ src/sql/sta/neuFoI.sql
@@ -1,8 +1,11 @@
 
+
 DROP VIEW IF EXISTS "FEATURES" CASCADE;
 CREATE OR REPLACE VIEW "FEATURES" AS
- EXPLAIN ANALYZE
+ --EXPLAIN ANALYZE
 SELECT
+    -- Zusammenbau der FoI_id: Hash aus Koordinaten und Aktion_ID (muss berechenbar sein,
+    -- damit sie auch unabhängig dem observation_table zur Verfügung steht)
    ('x' || MD5(crd.coordinates::text || crd.action_id))::bit(63)::bigint AS "ID",
  	CONCAT(c.label, '_', dsl.begin_date) AS "NAME",
 	crd.action_type AS "DESCRIPTION",
@@ -14,26 +17,16 @@ SELECT
 			'coordinates',jsonb_build_array(crd.coordinates)
 			)
 		) AS "FEATURE",
-	jsonb_build_object()  AS "PROPERTIES",
-
-crd.action_id AS crd_action_id,
--- crd.akt_dma_id AS crd_akt_dma_id,
---crd.dyn_dma_id AS crd_dyn_dma_id,
-dma.id AS foi_dma
-
+	jsonb_build_object()  AS "PROPERTIES"
 
 
 
 FROM public.sms_datastream_link dsl
 JOIN public.sms_device_mount_action dma ON dma.id = dsl.device_mount_action_id
--- LEFT JOIN public.sms_configuration_dynamic_location_begin_action dla ON dla.configuration_id = dma.configuration_id
--- LEFT JOIN public.sms_configuration_static_location_begin_action sla ON sla.configuration_id = dma.configuration_id
 JOIN public.sms_configuration c ON c.id =dma.configuration_id
 JOIN public.sms_device d ON d.id = dma.device_id
---JOIN vo_demogroup_887a7030491444e0aee126fbc215e9f7.observation o ON o.datastream_id = dsl.datastream_id
-JOIN v2_ts_coordinates crd ON crd.datastream_id = dsl.datastream_id --AND crd.akt_dma_id = crd.action_id
-    WHERE dma.id = crd.action_id  OR dma.id = crd.dyn_dma_id
-
---WHERE crd.action_type = 'static'
---WHERE dsl.datasource_id = 'vo_demogroup_887a7030491444e0aee126fbc215e9f7' -- AND c.is_public AND d.is_public
+JOIN ts_coordinates crd ON crd.datastream_id = dsl.datastream_id
+    -- Bedingung, dma_id entweder für den Fall 'static' mit action_id aus CTE 'ts_action_type' identisch (entspricht der sla_id)
+-- oder für den Fall 'dynamic' mit der
+    WHERE dma.id = crd.action_id OR dma.id = crd.dyn_dma_id
 GROUP BY crd.action_type, crd.coordinates, crd.action_id, c."label", dsl.begin_date, dma.id, crd.dyn_dma_id;
