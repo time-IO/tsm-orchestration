831b3e377a67118122143bbfbab80e0fea5be9bc
David Schaefer <david.schaefer@ufz.de>
2025-09-10 00:05:46 +0200

    migration

--- flyway/migrations/thing-management/V2_0_3__alter_rawdatastorage.sql
+++ flyway/migrations/thing-management/V2_0_3__alter_rawdatastorage.sql
@@ -1,2 +1,3 @@
 ALTER TABLE thing DROP COLUMN IF EXISTS enable_raw_data_storage;
-ALTER TABLE thing ADD COLUMN IF NOT EXISTS filename_pattern text;
+ALTER TABLE external_sftp_ingest DROP COLUMN IF EXISTS file_name_pattern;
+ALTER TABLE rawdatastorage ADD COLUMN IF NOT EXISTS file_name_pattern text;
