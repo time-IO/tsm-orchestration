859fec3f12779aa4e0e4237cc2392647f45c4bea
Johanna Mardt <johanna.mardt@ufz.de>
2025-09-10 11:21:10 +0200

    wichtige Änderung in ts_coordinates, DISTINCT ON sla-dma_id, nicht sla_id

--- src/sql/sta/Helper-Views/ts_coordinates.sql
+++ src/sql/sta/Helper-Views/ts_coordinates.sql
@@ -1,107 +1,65 @@
-
--- Helper_view "ts_coordinates" ermittelt die Koordinaten aufgeschlüsselt nach static oder dynamic
-
 DROP VIEW IF EXISTS ts_coordinates CASCADE;
-CREATE OR REPLACE VIEW ts_coordinates  AS
--- Da dür die dynamische Location die x-, y- und z-Koordinate in einzelnen Observations gespeichert werden,
--- wird für jede Koordinate eine eigene CTE für die Abfrage der betreffenden Observation gestartet.
--- Die Abfrage läuft über sms_datastream_link gebunden an die device_mount_action_id, die configuration_id und die datastream_id
-   EXPLAIN ANALYZE
-        WITH
-        -- CTE für dynamische x-Koordinate
-        xkoordinaten AS (
-            SELECT
+CREATE OR REPLACE VIEW ts_coordinates AS
+--     View zur Bestimmung der Koordinaten, Unterscheidung der static/dynamic actions,
+--     später durch UNION wieder zusammengefügt, daher gleicher Aufbau (columns)
 
-                o.result_time,
-                  o.result_number AS x_koor,
-                dma.id AS dyn_dma_id --  device_mount_action_id für dynamic, da hier an dla gebunden
-            FROM sms_datastream_link dsl
-            JOIN sms_device_mount_action dma ON dma.id = dsl.device_mount_action_id
-            JOIN sms_configuration_dynamic_location_begin_action dla ON dla.configuration_id = dma.configuration_id
-            JOIN  vo_demogroup_887a7030491444e0aee126fbc215e9f7.observation o ON o.datastream_id = dsl.datastream_id
-            -- Bedingung für x-Koordinate
-            WHERE dsl.device_property_id = dla.x_property_id
-        ),
+WITH
 
-        -- CTE für dynamische y-Koordinate
-        ykoordinaten AS (
-            SELECT
+static_coords AS (SELECT DISTINCT ON (stat_dma_id)
+--    -- beschränkter Distinct, da bei static ein FoI für eine sla.id
+                      'static'      AS action_type,
+                       ts.action_id,
+                       ts.datastream_id,
+                       dma.id        AS stat_dma_id,
+                       NULL::integer AS dyn_dma_id, -- Leerstelle für dyn_dma_id
 
-                o.result_time,
-                o.result_number AS y_koor,
-                --dsl.datastream_id AS y_datastream,
-                dla.y_property_id
-            FROM sms_datastream_link dsl
-            JOIN sms_device_mount_action dma ON dma.id = dsl.device_mount_action_id
-            JOIN sms_configuration_dynamic_location_begin_action dla ON dla.configuration_id = dma.configuration_id
-            JOIN  vo_demogroup_887a7030491444e0aee126fbc215e9f7.observation o ON o.datastream_id = dsl.datastream_id
-            -- Bedingung für y-Koordinate
-            WHERE dsl.device_property_id = dla.y_property_id
-        ),
+                         CASE
+                            WHEN sla.z IS NULL THEN ARRAY [sla.x, sla.y]
+                            ELSE ARRAY [sla.x, sla.y, sla.z]
+                         END       AS coordinates
 
-        -- CTE für dynamische Z-Koordinate
-        zkoordinaten AS (
-            SELECT
-
-                o.result_time,
-                o.result_number AS z_koor
-            FROM sms_datastream_link dsl
-            JOIN sms_device_mount_action dma ON dma.id = dsl.device_mount_action_id
-            JOIN sms_configuration_dynamic_location_begin_action dla ON dla.configuration_id = dma.configuration_id
-            JOIN  vo_demogroup_887a7030491444e0aee126fbc215e9f7.observation o ON o.datastream_id = dsl.datastream_id
-            WHERE dsl.device_property_id = dla.z_property_id
-            -- Bedingung für z-Koordinate
-        ),
+                  FROM ts_action_type ts
+                           LEFT JOIN sms_configuration_static_location_begin_action sla ON sla.id = ts.action_id
+                           JOIN sms_device_mount_action dma ON dma.configuration_id = sla.configuration_id
+                  WHERE ts.action_type = TRUE),
 
 
-        -- CTE Abfrage der Helper-View ts_action_type, inkl. Bestimmung der dma_id für static
-        aktionen AS (
-            SELECT
+-- CTE um die Koordinaten der dynamic actions zu bestimmten (WHERE at.action_type = FALSE),
+    -- Zugriff auf die einzelnen Helper-Views für die x/y/z- Koordinaten, über result-time
+dynamic_coords AS (SELECT 'dynamic'     AS action_type,
+                          at.action_id::int,
+                          at.datastream_id,
+                          NULL::integer AS stat_dma_id, --Leerstelle für stat_dma_id
+                          x.dyn_dma_id,
+                          CASE
+                              WHEN z.z_koor IS NULL THEN ARRAY [x.x_koor, y.y_koor]
+                              ELSE ARRAY [x.x_koor, y.y_koor, z.z_koor]
+                              END       AS coordinates
 
-                ts.result_time,
-                ts.action_type,
-                ts.action_id, -- sla/oder dla.id
---                 ts.observation_id,
-                ts.datastream_id,
-                dma.configuration_id,
-                dma.id AS stat_dma_id -- dma_id nur für static, nicht für dyn. -> hier falsche Ausgabe (JOIN ... ON sla.configuration_id)
-            FROM ts_action_type ts
-            JOIN sms_configuration_static_location_begin_action sla ON sla.id = ts.action_id
-            JOIN sms_device_mount_action dma ON dma.configuration_id = sla.configuration_id
-            WHERE ts.action_id = dma.id
-        )
-
-
-        -- Hauptabfrage, Koordinatenabfrage über CTEs
-        SELECT DISTINCT
-            a.action_type,
-            a.action_id,
-            a.datastream_id,
-            x.dyn_dma_id,
-            sla.id,
-            a.stat_dma_id,
-
-            CASE
-                WHEN a.action_type = TRUE THEN
-                    CASE
-                        WHEN sla.z IS NULL THEN ARRAY[sla.x, sla.y]
-                        ELSE ARRAY[sla.x, sla.y, sla.z]
-                    END
-                WHEN a.action_type = FALSE THEN
-                    CASE
-                        WHEN z.z_koor IS NULL THEN ARRAY[x.x_koor, y.y_koor]
-                        ELSE ARRAY[x.x_koor, y.y_koor, z.z_koor]
-                    END
-            END AS coordinates
-
-        FROM aktionen a
-        LEFT JOIN xkoordinaten x ON x.result_time = a.result_time
-        LEFT JOIN ykoordinaten y ON y.result_time = a.result_time
-        LEFT JOIN zkoordinaten z ON z.result_time = a.result_time
+                   FROM ts_action_type at
+                            LEFT JOIN ts_coordinates_x_koor x ON x.result_time = at.result_time
+                            LEFT JOIN ts_coordinates_y_koor y ON y.result_time = at.result_time
+                            LEFT JOIN ts_coordinates_z_koor z ON z.result_time = at.result_time
+                   WHERE at.action_type = FALSE
+                    AND at.datastream_id IN (x.datastream_id, y.datastream_id, z.datastream_id)
+    )
 
 
+SELECT action_type,
+       action_id,
+       datastream_id,
+       dyn_dma_id,
+       stat_dma_id,
+       coordinates
+FROM static_coords
 
-            -- damit keine Dopplung der Daten, müssen sowohl die sla_id als auch die dma_id aus 'aktionen' mit der action_id aus 'aktionen'
-            -- übereinstimmen (letztere steht für die sla_id bei Überprüfung des Types)
-          LEFT JOIN sms_configuration_static_location_begin_action sla ON sla.configuration_id = a.configuration_id AND sla.id = a.action_id
---             WHERE a.action_id = a.stat_dma_id
+UNION ALL
+
+SELECT action_type,
+       action_id,
+       datastream_id,
+       dyn_dma_id,
+       stat_dma_id,
+       coordinates
+FROM dynamic_coords;
+
