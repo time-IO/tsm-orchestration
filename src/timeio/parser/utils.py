70cd8f71497956f51fae312c107066f8db61943c
joost.hemmen1 <joost.hemmen@ufz.de>
2025-09-16 09:58:51 +0200

    fix import

--- src/timeio/parser/utils.py
+++ src/timeio/parser/utils.py
@@ -6,7 +6,7 @@ from timeio.parser.mqtt.parser import MqttParser
 from timeio.parser.mqtt.devices.brightsky_dwd import BrightskyDwdApiParser
 from timeio.parser.mqtt.devices.campbell_cr6 import CampbellCr6Parser
 from timeio.parser.mqtt.devices.chirpstack_generic import ChirpStackGenericParser
-from timeio.parser.mqtt.devices.ydoc_ml417 import YdocMl417Parser
+from timeio.parser.mqtt.devices.ydoc_ml_417 import YdocMl417Parser
 from timeio.parser.mqtt.devices.sine_dummy import SineDummyParser
 
 
