717272cc90285aebf94b8867db8a052fd5c95f54
joost.hemmen1 <joost.hemmen@ufz.de>
2025-10-20 16:09:02 +0200

    split into mqtt and userinfo class. pass connection as envvars

---
+++ keycloak/mqtt-notifier/src/main/java/com/timeio/mqtt/MQTTService.java
@@ -0,0 +1,65 @@
+package com.timeio.mqtt;
+
+import org.eclipse.paho.client.mqttv3.*;
+import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
+import org.jboss.logging.Logger;
+
+public class MQTTService {
+    private static final Logger LOG = Logger.getLogger(MQTTService.class);
+
+    private static final String MQTT_BROKER = System.getenv().getOrDefault("MQTT_BROKER", "tcp://mqtt-broker:1883");
+    private static final String MQTT_USER = System.getenv().getOrDefault("MQTT_USER", "mqtt");
+    private static final String MQTT_PASSWORD = System.getenv().getOrDefault("MQTT_PASSWORD", "mqtt");
+    private static final String MQTT_TOPIC = System.getenv().getOrDefault("MQTT_TOPIC", "user_login");
+    private static final int MQTT_QOS = Integer.parseInt(System.getenv().getOrDefault("MQTT_QOS", "2"));
+
+    private static final MQTTService INSTANCE = new MQTTService();
+
+    private MqttClient mqttClient;
+
+    private MQTTService() {
+        try {
+            mqttClient = new MqttClient(MQTT_BROKER, MqttClient.generateClientId(), new MemoryPersistence());
+            MqttConnectOptions options = new MqttConnectOptions();
+            options.setUserName(MQTT_USER);
+            options.setPassword(MQTT_PASSWORD.toCharArray());
+            options.setAutomaticReconnect(true);
+            options.setCleanSession(true);
+
+            mqttClient.setCallback(new MqttCallbackExtended() {
+                public void connectComplete(boolean reconnect, String brokerURI) {
+                    LOG.debugf("MQTT connected%s: %s", reconnect ? " (Reconnect)" : "", brokerURI);
+                }
+                public void connectionLost(Throwable cause) {
+                    LOG.warn("MQTT connection lost", cause);
+                }
+                public void messageArrived(String topic, MqttMessage message) {}
+                public void deliveryComplete(IMqttDeliveryToken token) {}
+            });
+
+            mqttClient.connect(options);
+            LOG.info("Connection to MQTT-Broker established");
+        } catch (Exception e) {
+            LOG.error("Error when connecting to MQTT-Broker", e);
+        }
+    }
+
+    public static MQTTService getInstance() {
+        return INSTANCE;
+    }
+
+    public void publish(String payload) {
+        try {
+            if (mqttClient != null && mqttClient.isConnected()) {
+                MqttMessage message = new MqttMessage(payload.getBytes());
+                message.setQos(MQTT_QOS);
+                mqttClient.publish(MQTT_TOPIC, message);
+                LOG.debugf("MQTT message sent to topic '%s': %s", MQTT_TOPIC, payload);
+            } else {
+                LOG.warn("MQTT-Client not connected, message not sent");
+            }
+        } catch (Exception e) {
+            LOG.error("Error while sending MQTT Message", e);
+        }
+    }
+}
--- keycloak/mqtt-notifier/src/main/java/com/timeio/mqtt/UserInfoResponseFilter.java
+++ keycloak/mqtt-notifier/src/main/java/com/timeio/mqtt/UserInfoResponseFilter.java
@@ -4,10 +4,6 @@ import jakarta.ws.rs.container.ContainerRequestContext;
 import jakarta.ws.rs.container.ContainerResponseContext;
 import jakarta.ws.rs.container.ContainerResponseFilter;
 import jakarta.ws.rs.ext.Provider;
-import org.eclipse.paho.client.mqttv3.MqttClient;
-import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
-import org.eclipse.paho.client.mqttv3.MqttMessage;
-import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
 import org.jboss.logging.Logger;
 
 import java.io.IOException;
@@ -16,31 +12,7 @@ import java.io.IOException;
 public class UserInfoResponseFilter implements ContainerResponseFilter {
 
     private static final Logger LOG = Logger.getLogger(UserInfoResponseFilter.class);
-
-    // Hardcodierte MQTT-Settings
-    private static final String MQTT_BROKER = "tcp://mqtt-broker:1883";
-    private static final String MQTT_USER = "mqtt";
-    private static final String MQTT_PASSWORD = "mqtt";
-    private static final String MQTT_TOPIC = "user_login";
-
-    private MqttClient mqttClient;
-
-    public UserInfoResponseFilter() {
-        try {
-            mqttClient = new MqttClient(MQTT_BROKER, MqttClient.generateClientId(), new MemoryPersistence());
-            MqttConnectOptions options = new MqttConnectOptions();
-            options.setUserName(MQTT_USER);
-            options.setPassword(MQTT_PASSWORD.toCharArray());
-            options.setAutomaticReconnect(true);
-            options.setCleanSession(true);
-
-            mqttClient.connect(options);
-            LOG.info("=== MQTT-Verbindung hergestellt ===");
-
-        } catch (Exception e) {
-            LOG.error("Fehler beim Verbinden mit MQTT-Broker", e);
-        }
-    }
+    private final MQTTService mqttService = MQTTService.getInstance();
 
     @Override
     public void filter(ContainerRequestContext requestContext,
@@ -49,34 +21,18 @@ public class UserInfoResponseFilter implements ContainerResponseFilter {
         String path = requestContext.getUriInfo().getPath();
 
         if (path.contains("/protocol/openid-connect/userinfo")) {
-            LOG.infof("=== USERINFO RESPONSE ===");
-            LOG.infof("Status: %d", responseContext.getStatus());
+            LOG.debugf("=== USERINFO RESPONSE ===");
+            LOG.debugf("Status: %d", responseContext.getStatus());
 
             Object entity = responseContext.getEntity();
             if (entity != null) {
                 String userInfoJson = entity.toString();
-                LOG.infof("UserInfo JSON: %s", userInfoJson);
+                LOG.debugf("UserInfo JSON: %s", userInfoJson);
 
-                // MQTT-Publish
-                publishToMqtt(userInfoJson);
+                mqttService.publish(userInfoJson);
             } else {
-                LOG.warn("UserInfo Response body ist leer");
+                LOG.warn("UserInfo response body is empty");
             }
-        }
-    }
-
-    private void publishToMqtt(String payload) {
-        try {
-            if (mqttClient != null && mqttClient.isConnected()) {
-                MqttMessage message = new MqttMessage(payload.getBytes());
-                message.setQos(1);
-                mqttClient.publish(MQTT_TOPIC, message);
-                LOG.infof("MQTT-Nachricht gesendet an Topic '%s': %s", MQTT_TOPIC, payload);
-            } else {
-                LOG.warn("MQTT-Client nicht verbunden, Nachricht nicht gesendet");
-            }
-        } catch (Exception e) {
-            LOG.error("Fehler beim Senden der MQTT-Nachricht", e);
         }
     }
 }
