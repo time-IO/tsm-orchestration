1f8408dd42b0c477de0741cd9c1a71ad4624c05f
joost.hemmen1 <joost.hemmen@ufz.de>
2025-10-23 11:06:47 +0200

    clean up worker_launcher

--- src/worker_launcher.sh
+++ src/worker_launcher.sh
@@ -12,34 +12,24 @@ else
   exit 1
 fi
 
-attempts=0
-start_time=$(date +%s)
-
-while true; do
-  echo "Attempt $((attempts + 1)) of ${MAX_ATTEMPTS} to start container with command: $CMD"
+attempts=1
+while [[ $attempts -le $MAX_ATTEMPTS ]]; do
+  start=$(date +%s)
+  echo "Attempt $((attempts)) of ${MAX_ATTEMPTS} to start container with command: $CMD"
   $CMD && exit 0
-
+  ((attempts++))
   # Increment attempts and check time window
-  attempts=$((attempts + 1))
-  now=$(date +%s)
-  elapsed=$((now - start_time))
-
+  end=$(date +%s)
+  elapsed=$((end-start))
 
   # only count as failed attempt if within the window
-  if [[ $elapsed -le $WINDOW_SECONDS ]]; then
-    if [[ $attempts -eq $MAX_ATTEMPTS ]]; then
-      echo "Error: Command failed after $attempts attempts within the first $WINDOW_SECONDS seconds."
-      # We exit with a non-zero status to not restart the container.
-      exit 0
-    fi
-  else
+  if [[ $elapsed -gt $WINDOW_SECONDS ]]; then
     # reset attempts and timer if outside the window
     echo "Failed attempt outside of $WINDOW_SECONDS seconds window. Resetting attempt counter."
-    attempts=0
-    start_time=$now
+    attempts=1
   fi
 done
 
-echo "MAX_ATTEMPTS: $MAX_ATTEMPTS"
-echo "WINDOW_SECONDS: $WINDOW_SECONDS"
-echo "CMD: $CMD"
+echo "Error: Command failed after $attempts attempts, each within the first $WINDOW_SECONDS seconds."
+# We exit with a non-zero status to not restart the container.
+exit 0
