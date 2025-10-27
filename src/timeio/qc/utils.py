5e09457fa7b3bebfdcf2fd4e633c53f52505d548
Bert Palm <bert.palm@ufz.de>
2025-10-27 17:14:11 +0100

    simplified parsing

--- src/timeio/qc/utils.py
+++ src/timeio/qc/utils.py
@@ -37,12 +37,19 @@ def collect_tests(conf: feta.QAQC) -> list[QcTest]:
     tests = []
     for i, test in enumerate(conf.get_tests()):  # type: int, feta.QAQCTest
         params = collect_params(test)
-        qctest = QcTest(
-            name=test.name,
-            func_name=test.function,
-            params=params,
-            context_window=context_window,
-            qctool="saqc",
-        )
+        name = test.name
+        try:
+            qctest = QcTest(
+                name=name,
+                func_name=test.function,
+                params=params,
+                context_window=context_window,
+                qctool="saqc",
+            )
+        except (NotImplementedError, ValueError) as e:
+            e.add_note(f"Qc test {name=} {i=}")
+            e.add_note(f"Config {conf}")
+            raise e
+
         tests.append(qctest)
     return tests
