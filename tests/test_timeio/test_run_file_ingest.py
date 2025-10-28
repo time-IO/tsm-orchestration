eddb03c2034953258f62a22c26bf6a506e2dc472
Bert Palm <bert.palm@ufz.de>
2025-10-28 23:08:07 +0100

    added tests for file_ingest minor refactoring

---
+++ tests/test_timeio/test_run_file_ingest.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+import pytest
+
+from run_file_ingest import ParserJobHandler
+
+
+@pytest.mark.parametrize(
+    "content, expected",
+    [
+        ({"EventName": "s3:something"}, False),
+        ({"EventName": "s3:ObjectCreated:Put"}, True),
+        ({"EventName": "s3:ObjectCreated:CompleteMultipartUpload"}, True),
+    ],
+)
+def test__ParserJobHandler_is_valid_event(content, expected):
+    result = ParserJobHandler.is_valid_event(content)
+    assert result == expected
+
+
+@pytest.mark.parametrize(
+    "content, expected",
+    [
+        ({}, KeyError("EventName")),
+        (None, TypeError("'NoneType' object is not subscriptable")),
+    ],
+)
+def test__ParserJobHandler_is_valid_event__raises(content, expected):
+    with pytest.raises(type(expected), match=str(expected)):
+        ParserJobHandler.is_valid_event(content)
