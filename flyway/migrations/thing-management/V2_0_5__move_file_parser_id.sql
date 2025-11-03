086c3e5af6fd2c3cb4311b4b7665414b6eeeeeaa
granseef <florian.gransee@ufz.de>
2025-11-03 12:48:52 +0100

    make file-parser-id nullable in rawdatastorage

--- flyway/migrations/thing-management/V2_0_5__move_file_parser_id.sql
+++ flyway/migrations/thing-management/V2_0_5__move_file_parser_id.sql
@@ -1,4 +1,5 @@
 ALTER TABLE external_sftp_ingest DROP COLUMN IF EXISTS file_parser_id;
 ALTER TABLE rawdatastorage ADD COLUMN IF NOT EXISTS file_parser_id INT;
+ALTER TABLE rawdatastorage ALTER COLUMN file_parser_id DROP NOT NULL;
 
-ALTER TABLE rawdatastorage ADD CONSTRAINT fk_file_parser FOREIGN KEY (file_parser_id) REFERENCES file_parser(id) ON DELETE CASCADE DEFERRABLE INITIALLY DEFERRED;
+ALTER TABLE rawdatastorage ADD CONSTRAINT fk_file_parser FOREIGN KEY (file_parser_id) REFERENCES file_parser(id) ON DELETE SET NULL DEFERRABLE INITIALLY DEFERRED;
