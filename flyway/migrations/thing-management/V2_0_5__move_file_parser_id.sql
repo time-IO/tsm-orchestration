b243a94b6f821b018f6f346a855408385f8dbd5b
granseef <florian.gransee@ufz.de>
2025-11-03 12:33:45 +0100

    add fk constraint

--- flyway/migrations/thing-management/V2_0_5__move_file_parser_id.sql
+++ flyway/migrations/thing-management/V2_0_5__move_file_parser_id.sql
@@ -1,2 +1,4 @@
 ALTER TABLE external_sftp_ingest DROP COLUMN IF EXISTS file_parser_id;
 ALTER TABLE rawdatastorage ADD COLUMN IF NOT EXISTS file_parser_id INT;
+
+ALTER TABLE rawdatastorage ADD CONSTRAINT fk_file_parser FOREIGN KEY (file_parser_id) REFERENCES file_parser(id) ON DELETE CASCADE DEFERRABLE INITIALLY DEFERRED;
