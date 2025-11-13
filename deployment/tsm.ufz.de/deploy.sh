10d36b289e618e8fe2eee83f8f23724e8c200616
joost.hemmen1 <joost.hemmen@ufz.de>
2025-11-13 11:31:25 +0100

    replace --build with --quiet-pull

--- deployment/tsm.ufz.de/deploy.sh
+++ deployment/tsm.ufz.de/deploy.sh
@@ -22,7 +22,7 @@ rm remove-all-data.sh
 COMPOSE_FILES="-f docker-compose.yml -f docker-compose.override.prod.yml"
 ENV_FILES="--env-file .env --env-file releases/release.env"
 # ... otherwise deploy time.IO with the tag env file
-sudo docker compose $COMPOSE_FILES $ENV_FILES create --build
+sudo docker compose $COMPOSE_FILES $ENV_FILES create --quiet-pull
 sudo docker compose $COMPOSE_FILES $ENV_FILES up -d
 sleep 10
 sudo docker compose $COMPOSE_FILES $ENV_FILES ps
