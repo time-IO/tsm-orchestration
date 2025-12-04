5811b5be38c01e251793fd77b2e0a3d3e9bb4567
granseef <florian.gransee@ufz.de>
2025-12-04 13:47:57 +0100

    add uuid column to configdb file parser

---
+++ flyway/migrations/public/V2_25__add_uuid_to_parser.sql
@@ -0,0 +1,6 @@
+ALTER TABLE config_db.file_parser ADD COLUMN uuid UUID;
+UPDATE config_db.file_parser set uuid = gen_random_uuid() WHERE uuid IS NULL;
+ALTER TABLE config_db.file_parser ALTER COLUMN uuid set NOT NULL;
+ALTER TABLE config_db.file_parser ALTER COLUMN uuid SET DEFAULT gen_random_uuid();
+ALTER TABLE config_db.file_parser ADD CONSTRAINT parser_uuid_unique UNIQUE (uuid);
+
