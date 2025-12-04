96c0cf3cc39712e7ae9dd17e2d1243d063215484
granseef <florian.gransee@ufz.de>
2025-12-04 13:48:45 +0100

    remove empty lines

--- flyway/migrations/public/V2_25__add_uuid_to_parser.sql
+++ flyway/migrations/public/V2_25__add_uuid_to_parser.sql
@@ -3,4 +3,3 @@ UPDATE config_db.file_parser set uuid = gen_random_uuid() WHERE uuid IS NULL;
 ALTER TABLE config_db.file_parser ALTER COLUMN uuid set NOT NULL;
 ALTER TABLE config_db.file_parser ALTER COLUMN uuid SET DEFAULT gen_random_uuid();
 ALTER TABLE config_db.file_parser ADD CONSTRAINT parser_uuid_unique UNIQUE (uuid);
-
