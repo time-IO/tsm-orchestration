59c126363272c95f82042baa7dd7954f645f06d4
granseef <florian.gransee@ufz.de>
2025-12-05 11:56:15 +0100

    add missing values to tmm

--- flyway/migrations/thing-management/V2_0_7__add_uuid_to_file_parser.sql
+++ flyway/migrations/thing-management/V2_0_7__add_uuid_to_file_parser.sql
@@ -3,3 +3,10 @@ ALTER TABLE file_parser ADD COLUMN uuid UUID NOT NULL UNIQUE;
 ALTER TABLE file_parser ADD CONSTRAINT file_parser_name_project_uniq UNIQUE ("name", project_id);
 
 ALTER TABLE mqtt_ingest ALTER COLUMN uri DROP NOT NULL;
+
+INSERT INTO external_api_type (name)
+VALUES ('uba'::varchar(200)),
+       ('tsystems'::varchar(200));
+
+INSERT INTO mqtt_device_type (name)
+VALUES ('chirpstack_generic'::varchar(200));
