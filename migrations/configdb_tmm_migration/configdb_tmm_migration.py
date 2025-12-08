d3388cb236c95c841a22db1859c7db498d855490
granseef <florian.gransee@ufz.de>
2025-12-08 18:39:17 +0100

    add first implementation for projects, db, parser, things, ingests

---
+++ migrations/configdb_tmm_migration/configdb_tmm_migration.py
@@ -0,0 +1,164 @@
+import psycopg
+from psycopg.rows import dict_row
+from psycopg.types.json import Json
+
+from sql import queries
+
+
+def get_django_things(django_cur):
+    django_cur.execute("SELECT thing_id FROM frontenddb.tsm_thing")
+    rows = django_cur.fetchall()
+
+    return {r["thing_id"] for r in rows}
+
+
+def migrate_project_and_db(cfgdb_cur, tmm_cur):
+    cfgdb_cur.execute(queries.SELECT_PROJECT_AND_DB)
+    rows = cfgdb_cur.fetchall()
+
+    tmm_cur.execute("SELECT uuid, database_id FROM thing_management_db.project")
+    existing_projects = {r["uuid"] for r in tmm_cur.fetchall()}
+
+    for row in rows:
+        if row["project_uuid"] in existing_projects:
+            continue
+        row["project_name"] = row["project_name"].split(":")[1]
+
+        tmm_cur.execute(queries.INSERT_DB, row)
+        db_id = tmm_cur.fetchone()["id"]
+        row["project_database_id"] = db_id
+        tmm_cur.execute(queries.INSERT_PROJECT, row)
+
+
+def migrate_parsers(cfgdb_cur, tmm_cur, django_things):
+    tmm_cur.execute("SELECT id, uuid from thing_management_db.project")
+    projects = tmm_cur.fetchall()
+    projects = {p["uuid"]: p["id"] for p in projects}
+    cfgdb_cur.execute(queries.GET_PARSER)
+    rows = cfgdb_cur.fetchall()
+    valid_rows = []
+    for row in rows:
+        if row["thing_uuid"] in django_things:
+            row["project_id"] = projects.get(row["project_uuid"])
+            # hard_coded because only one entry in config_db (csvparser)
+            row["file_parser_type_id"] = 1
+            # TODO
+            row["created_by"] = 1
+            adapt_json_fields(row)
+            valid_rows.append(row)
+    tmm_cur.executemany(queries.INSERT_PARSER, valid_rows)
+
+
+def map_ids_by_name(cfgdb_cur, tmm_cur, cfgdb_query, tmm_query):
+    cfgdb_cur.execute(cfgdb_query)
+    tmm_cur.execute(tmm_query)
+    cfgdb_rows = cfgdb_cur.fetchall()
+    tmm_rows = tmm_cur.fetchall()
+    cfgdb_by_name = {row["name"]: row["id"] for row in cfgdb_rows}
+    tmm_by_name = {row["name"]: row["id"] for row in tmm_rows}
+
+    return {cfgdb_id: tmm_by_name[name] for name, cfgdb_id in cfgdb_by_name.items()}
+
+
+def map_ingest_types(cfgdb_cur, tmm_cur):
+    return map_ids_by_name(
+        cfgdb_cur,
+        tmm_cur,
+        'SELECT id, "name" FROM config_db.ingest_type',
+        'SELECT id, "name" FROM thing_management_db.ingest_type',
+    )
+
+
+def map_extapi_types(cfgdb_cur, tmm_cur):
+    return map_ids_by_name(
+        cfgdb_cur,
+        tmm_cur,
+        'SELECT id, "name" FROM config_db.ext_api_type',
+        'SELECT id, "name" FROM thing_management_db.external_api_type',
+    )
+
+
+def map_mqtt_device_types(cfgdb_cur, tmm_cur):
+    return map_ids_by_name(
+        cfgdb_cur,
+        tmm_cur,
+        'SELECT id, "name" FROM config_db.mqtt_device_type',
+        'SELECT id, "name" FROM thing_management_db.mqtt_device_type',
+    )
+
+
+def adapt_json_fields(row):
+    for k, v in row.items():
+        if isinstance(v, dict):
+            row[k] = Json(v)
+    return row
+
+
+def migrate_thing_and_ingest(cfgdb_cur, tmm_cur, django_things):
+    tmm_cur.execute("SELECT uuid, id FROM thing_management_db.project")
+    project_uuid_to_id = {r["uuid"]: r["id"] for r in tmm_cur.fetchall()}
+
+    cfgdb_cur.execute(queries.SELECT_THING_AND_INGEST)
+    rows = cfgdb_cur.fetchall()
+
+    tmm_cur.execute('SELECT "uuid", id FROM thing_management_db.file_parser')
+    existing_parser = {r["uuid"]: r["id"] for r in tmm_cur.fetchall()}
+
+    ingest_id_mapping = map_ingest_types(cfgdb_cur, tmm_cur)
+    extapi_types_mapping = map_extapi_types(cfgdb_cur, tmm_cur)
+    mqtt_device_type_mapping = map_mqtt_device_types(cfgdb_cur, tmm_cur)
+
+    for row in rows:
+        if row["uuid"] not in django_things:
+            continue
+        row["project_id"] = project_uuid_to_id[row["project_uuid"]]
+        row["ingest_type_id"] = ingest_id_mapping[row["ingest_type_id"]]
+        # TODO
+        row["created_by"] = 1
+        row["ea_api_type_id"] = extapi_types_mapping.get(row["ea_api_type_id"])
+        row["mqtt_device_type_id"] = mqtt_device_type_mapping.get(
+            row["mqtt_device_type_id"]
+        )
+        row["file_parser_id"] = existing_parser.get(row["file_parser_uuid"])
+        adapt_json_fields(row)
+        tmm_cur.execute(queries.UPSERT_THING, row)
+        row["thing_id"] = tmm_cur.fetchone()["id"]
+        adapt_json_fields(row)
+        ingest_name = row["ingest_type_name"]
+        upsert_ingest_query = queries.INGEST_QUERIES[ingest_name]
+        tmm_cur.execute(upsert_ingest_query, row)
+
+
+def run_migration(configdb_conn, tmm_conn, django_conn):
+    try:
+        with tmm_conn.transaction():
+            cfgdb_cur = configdb_conn.cursor()
+            tmm_cur = tmm_conn.cursor()
+            django_cur = django_conn.cursor()
+            django_things = get_django_things(django_cur)
+            migrate_project_and_db(cfgdb_cur, tmm_cur)
+            migrate_parsers(cfgdb_cur, tmm_cur, django_things)
+            migrate_thing_and_ingest(cfgdb_cur, tmm_cur, django_things)
+    except Exception as e:
+        print(f"migration failed: {e}")
+        configdb_conn.rollback()
+        tmm_conn.rollback()
+        django_conn.rollback()
+        raise
+
+
+if __name__ == "__main__":
+
+    # add respective DSN Entries here for accessing PROD DB!
+    # Later we can use the same DSN for cfgdb and tmm. But for testing you can requets PROD config_db schema and write
+    # into local thing_management_db schema (same for django DB)
+    # currently user_id/created_by  is hardcoded to "1" for thing and parser
+    cfgdb_dsn = "postgresql://postgres:postgres@localhost:5432/postgres"
+    tmm_dsn = "postgresql://postgres:postgres@localhost:5432/postgres"
+    django_dsn = "postgresql://frontenddb:frontenddb@localhost:5432/postgres"
+
+    cfgdb_conn = psycopg.connect(cfgdb_dsn, row_factory=dict_row)
+    tmm_conn = psycopg.connect(tmm_dsn, row_factory=dict_row)
+    django_conn = psycopg.connect(django_dsn, row_factory=dict_row)
+
+    run_migration(cfgdb_conn, tmm_conn, django_conn)
---
+++ migrations/configdb_tmm_migration/sql/queries.py
@@ -0,0 +1,146 @@
+INGEST_QUERIES = {
+    "extapi": """
+        INSERT INTO thing_management_db.external_api_ingest
+            (thing_id, api_type_id, sync_interval, sync_enabled, settings)
+        VALUES (%(id)s, %(ea_api_type_id)s, %(ea_sync_interval)s, %(ea_sync_enabled)s, %(ea_settings)s)
+        ON CONFLICT (thing_id) DO UPDATE SET sync_interval = EXCLUDED.sync_interval,
+                                             sync_enabled  = EXCLUDED.sync_enabled,
+                                             settings      = EXCLUDED.settings,
+                                             api_type_id   = EXCLUDED.api_type_id
+        """,
+    "extsftp": """
+        INSERT INTO thing_management_db.external_sftp_ingest
+        (thing_id, uri, path, "user", "password", ssh_priv_key, ssh_pub_key, sync_interval, sync_enabled)
+        VALUES (%(id)s, %(es_uri)s, %(es_path)s, %(es_user)s, %(es_password)s,
+                %(es_ssh_priv_key)s, %(es_ssh_pub_key)s, %(es_sync_interval)s, %(es_sync_enabled)s)
+        ON CONFLICT (thing_id) DO UPDATE SET uri           = EXCLUDED.uri,
+                                             path          = EXCLUDED.path,
+                                             "user"        = EXCLUDED."user",
+                                             "password"    = EXCLUDED."password",
+                                             ssh_priv_key  = EXCLUDED.ssh_priv_key,
+                                             ssh_pub_key   = EXCLUDED.ssh_pub_key,
+                                             sync_interval = EXCLUDED.sync_interval,
+                                             sync_enabled  = EXCLUDED.sync_enabled
+        """,
+    "mqtt": """
+        INSERT INTO thing_management_db.mqtt_ingest
+        (thing_id, "user", "password", password_hashed, topic, mqtt_device_type_id)
+        VALUES (%(id)s, %(mqtt_user)s, %(mqtt_password)s, %(mqtt_password_hashed)s,
+                %(mqtt_topic)s, %(mqtt_device_type_id)s)
+        ON CONFLICT (thing_id) DO UPDATE SET "user"            = EXCLUDED."user",
+                                             "password"        = EXCLUDED."password",
+                                             "password_hashed" = EXCLUDED.password_hashed,
+                                             topic             = EXCLUDED.topic
+        """,
+    "sftp": """
+        INSERT INTO thing_management_db.rawdatastorage
+        (thing_id, "user", "password", bucket_name, file_name_pattern, file_parser_id)
+        VALUES (%(id)s, %(s3_user)s, %(s3_password)s, %(s3_bucket)s, %(s3_filename_pattern)s, %(file_parser_id)s)
+        ON CONFLICT (thing_id) DO UPDATE SET "user"            = EXCLUDED."user",
+                                             "password"        = EXCLUDED."password",
+                                             bucket_name       = EXCLUDED.bucket_name,
+                                             file_name_pattern = EXCLUDED.file_name_pattern,
+                                             file_parser_id    = EXCLUDED.file_parser_id
+        """,
+}
+
+SELECT_PROJECT_AND_DB = """
+    SELECT p."name"      AS "project_name",
+        p.uuid        AS "project_uuid",
+        p.database_id AS "project_database_id",
+        db.schema,
+        db."user",
+        db."password",
+        db.ro_user,
+        db.ro_password
+    FROM config_db.project p
+    JOIN config_db.database db ON p.database_id = db.id
+"""
+
+INSERT_DB = """
+    INSERT INTO thing_management_db.database (db_schema, "user", "password", ro_user, ro_password)
+    VALUES (%(schema)s, %(user)s, %(password)s, %(ro_user)s, %(ro_password)s)
+    RETURNING id
+"""
+INSERT_PROJECT = """
+    INSERT INTO thing_management_db.project ("name", uuid, database_id)
+    VALUES (%(project_name)s, %(project_uuid)s, %(project_database_id)s); \
+"""
+
+GET_PARSER = """
+    SELECT fp.file_parser_type_id,
+        fp."name", \
+        fp.params, \
+        fp."uuid"  AS "parser_uuid", \
+        t.uuid   AS "thing_uuid", \
+        p."uuid" AS "project_uuid"
+    FROM config_db.file_parser fp
+    JOIN config_db.s3_store s3 ON s3.file_parser_id = fp.id
+    JOIN config_db.thing t ON t.s3_store_id = s3.id
+    JOIN config_db.project p ON p.id = t.project_id \
+"""
+
+INSERT_PARSER = """
+    INSERT INTO thing_management_db.file_parser
+        (file_parser_type_id, project_id, "name", settings, created_by, "uuid")
+    VALUES (%(file_parser_type_id)s, %(project_id)s, %(name)s, %(params)s, %(created_by)s, %(parser_uuid)s)
+    ON CONFLICT ("uuid") DO UPDATE SET settings = EXCLUDED.settings 
+"""
+
+SELECT_THING_AND_INGEST = """
+    SELECT t.id,
+        t."uuid",
+        t."name",
+        t.project_id,
+        p.uuid                AS "project_uuid",
+        t.ingest_type_id,
+        t.description,
+        it."name"             AS ingest_type_name,
+        ea.id                 AS ea_id,
+        ea.api_type_id        AS ea_api_type_id,
+        ea.sync_interval      AS ea_sync_interval,
+        ea.sync_enabled       AS ea_sync_enabled,
+        ea.settings           AS ea_settings,
+        es.id                 AS es_id,
+        es.uri                AS es_uri,
+        es."path"             AS es_path,
+        es."user"             AS es_user,
+        es."password"         AS es_password,
+        es.ssh_priv_key       AS es_ssh_priv_key,
+        es.ssh_pub_key        AS es_ssh_pub_key,
+        es.sync_interval      AS es_sync_interval,
+        es.sync_enabled       AS es_sync_enabled,
+        m.id                  AS mqtt_id,
+        m."user"              AS mqtt_user,
+        m."password"          AS mqtt_password,
+        m."password_hashed"   AS mqtt_password_hashed,
+        m.topic               AS mqtt_topic,
+        m.mqtt_device_type_id AS mqtt_device_type_id,
+        mdt."name"            AS "mqtt_device_type_name",
+        s3.id                 AS s3_id,
+        s3."user"             AS s3_user,
+        s3."password"         AS s3_password,
+        s3.bucket             AS s3_bucket,
+        s3.filename_pattern   AS s3_filename_pattern,
+        fp."uuid"             AS "file_parser_uuid"
+    FROM config_db.thing t
+    LEFT JOIN config_db.ext_api ea ON ea.id = t.ext_api_id
+    LEFT JOIN config_db.ext_sftp es ON es.id = t.ext_sftp_id
+    LEFT JOIN config_db.mqtt m ON m.id = t.mqtt_id
+    LEFT JOIN config_db.s3_store s3 ON s3.id = t.s3_store_id
+    JOIN config_db.ingest_type it ON it.id = t.ingest_type_id
+    JOIN config_db.project p ON p.id = t.project_id
+    LEFT JOIN config_db.mqtt_device_type mdt ON mdt.id = m.mqtt_device_type_id
+    LEFT JOIN config_db.file_parser fp ON fp.id = s3.file_parser_id
+"""
+
+UPSERT_THING = """
+    INSERT INTO thing_management_db.thing
+        (uuid, "name", description, project_id, ingest_type_id, created_by)
+    VALUES (%(uuid)s, %(name)s, %(description)s, %(project_id)s, %(ingest_type_id)s, %(created_by)s)
+    ON CONFLICT (uuid) DO UPDATE SET
+        "name"           = EXCLUDED."name",
+        "description"    = EXCLUDED."description",
+        "ingest_type_id" = EXCLUDED."ingest_type_id"
+    RETURNING id
+"""
