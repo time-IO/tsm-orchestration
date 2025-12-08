3309619847111715fdf5e4a832d59ba396a62900
granseef <florian.gransee@ufz.de>
2025-12-08 13:56:23 +0100

    add missing statement

--- flyway/migrations/thing-management/V2_0_7__add_uuid_to_file_parser.sql
+++ flyway/migrations/thing-management/V2_0_7__add_uuid_to_file_parser.sql
@@ -6,7 +6,7 @@ ALTER TABLE mqtt_ingest ALTER COLUMN uri DROP NOT NULL;
 
 ALTER TABLE thing ADD CONSTRAINT uuid_uniq UNIQUE (uuid);
 
-ALTER TABLE rawdatastorage ALTER COLUMN fileserver_uri
+ALTER TABLE rawdatastorage ALTER COLUMN fileserver_uri DROP NOT NULL;
 
 INSERT INTO external_api_type (name)
 VALUES ('uba'::varchar(200)),
