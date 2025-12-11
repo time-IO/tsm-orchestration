d360dd6ecc4f4342770c993b501fcdae28b85c9c
granseef <florian.gransee@ufz.de>
2025-12-11 15:29:35 +0100

    remove field and target

--- flyway/migrations/thing-management/V2_0_9__alter_qc_settings.sql
+++ flyway/migrations/thing-management/V2_0_9__alter_qc_settings.sql
@@ -1,8 +1,1 @@
-alter table qaqc_setting_function
-    add field jsonb;
-
-alter table qaqc_setting_function
-    add target jsonb;
-
-alter table qaqc_setting
-    drop column thing_id;
+ALTER TABLE qaqc_setting DROP COLUMN thing_id;
