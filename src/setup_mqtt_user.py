20c6cc54508fc88936b6af2631ee4180f09d10b7
Joost Hemmen <joost.hemmen@ufz.de>
2025-12-12 23:12:49 +0100

    use ReentrantConnection

--- src/setup_mqtt_user.py
+++ src/setup_mqtt_user.py
@@ -1,75 +1,33 @@
-from __future__ import annotations
+commit a4ab20b29c75f281e0546efda3a19444fd19aeee
+Author: Joost Hemmen <joost.hemmen@ufz.de>
+Date:   Fri Dec 12 23:10:09 2025 +0100
 
-import json
-import logging
+    fix mqtt setup and update dockercompose dev example
 
-import psycopg
-
-from timeio.mqtt import AbstractHandler, MQTTMessage
-from timeio.feta import Thing
-from timeio.common import get_envvar, setup_logging
-from timeio.journaling import Journal
-from timeio.typehints import MqttPayload
-
-logger = logging.getLogger("mqtt-user-setup")
-journal = Journal("System")
-
-
-class CreateMqttUserHandler(AbstractHandler):
-    def __init__(self):
-        super().__init__(
-            topic=get_envvar("TOPIC"),
-            mqtt_broker=get_envvar("MQTT_BROKER"),
-            mqtt_user=get_envvar("MQTT_USER"),
-            mqtt_password=get_envvar("MQTT_PASSWORD"),
-            mqtt_client_id=get_envvar("MQTT_CLIENT_ID"),
-            mqtt_qos=get_envvar("MQTT_QOS", cast_to=int),
-            mqtt_clean_session=get_envvar("MQTT_CLEAN_SESSION", cast_to=bool),
-        )
-        self.db = psycopg.connect(get_envvar("DATABASE_URL"))
-        self.configdb_dsn = get_envvar("CONFIGDB_DSN")
-
-    def act(self, content: MqttPayload.ConfigDBUpdate, message: MQTTMessage):
-        thing = Thing.from_uuid(content["thing"], dsn=self.configdb_dsn)
-        user = thing.mqtt.user
-        pw = thing.mqtt.password_hashed
-
-        logger.info(f"create user. {user=}")
-        created = self.create_user(thing, user, pw)
-        action = "Created" if created else "Updated"
-        journal.info(f"{action} MQTT user {user}", thing.uuid)
-
-    def create_user(self, thing, user, pw) -> bool:
-        """Returns True for insert and False for update"""
-        sql = (
-            "INSERT INTO mqtt_auth.mqtt_user (project_uuid, thing_uuid, username, "
-            "password, description, db_schema) "
-            "VALUES (%s, %s, %s, %s ,%s ,%s) "
-            "ON CONFLICT (thing_uuid) "
-            "DO UPDATE SET"
-            " project_uuid = EXCLUDED.project_uuid,"
-            " username = EXCLUDED.username,"
-            " password=EXCLUDED.password,"
-            " description = EXCLUDED.description,"
-            " db_schema = EXCLUDED.db_schema "
-            "RETURNING (xmax = 0)"
-        )
-        with self.db:
-            with self.db.cursor() as c:
-                c.execute(
-                    sql,
-                    (
-                        thing.project.uuid,
-                        thing.uuid,
-                        user,
-                        pw,
-                        thing.description,
-                        thing.database.username,
-                    ),
-                )
-                return c.fetchone()[0]
-
-
-if __name__ == "__main__":
-    setup_logging(get_envvar("LOG_LEVEL", "INFO"))
-    CreateMqttUserHandler().run_loop()
+diff --git a/src/setup_mqtt_user.py b/src/setup_mqtt_user.py
+index a0b9115..f635a1c 100755
+--- a/src/setup_mqtt_user.py
++++ b/src/setup_mqtt_user.py
+@@ -7,6 +7,7 @@ import psycopg
+ 
+ from timeio.mqtt import AbstractHandler, MQTTMessage
+ from timeio.feta import Thing
++from timeio.databases import ReentrantConnection
+ from timeio.common import get_envvar, setup_logging
+ from timeio.journaling import Journal
+ from timeio.typehints import MqttPayload
+@@ -26,10 +27,13 @@ class CreateMqttUserHandler(AbstractHandler):
+             mqtt_qos=get_envvar("MQTT_QOS", cast_to=int),
+             mqtt_clean_session=get_envvar("MQTT_CLEAN_SESSION", cast_to=bool),
+         )
+-        self.db = psycopg.connect(get_envvar("DATABASE_URL"))
++        self.db_conn = ReentrantConnection(get_envvar("DATABASE_URL"))
++        self.db = self.db_conn.connect()
+         self.configdb_dsn = get_envvar("CONFIGDB_DSN")
+ 
+     def act(self, content: MqttPayload.ConfigDBUpdate, message: MQTTMessage):
++        self.db = self.db_conn.connect()   
++
+         thing = Thing.from_uuid(content["thing"], dsn=self.configdb_dsn)
+         user = thing.mqtt.user
+         pw = thing.mqtt.password_hashed
