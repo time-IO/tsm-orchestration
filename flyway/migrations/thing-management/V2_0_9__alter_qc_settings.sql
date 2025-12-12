1f1c92aac0f125a5dda3a1a7e24caa4c6ef01dad
Tobias Kuhnert <tobias.kiuhnert@ufz.de>
2025-12-12 12:26:55 +0100

    remove function and trigger as they are not needed

--- flyway/migrations/thing-management/V2_0_9__alter_qc_settings.sql
+++ flyway/migrations/thing-management/V2_0_9__alter_qc_settings.sql
@@ -1,1 +1,7 @@
 ALTER TABLE qaqc_setting DROP COLUMN thing_id;
+
+-- Drop the trigger first (if it exists)
+DROP TRIGGER IF EXISTS trg_insert_function_parameters ON qaqc_setting_function;
+
+-- Drop the function (if it exists)
+DROP FUNCTION IF EXISTS insert_function_parameters();
