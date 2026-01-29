10b9992c8c1303279e107c5b72f982a35aeeb13c
Florian Gransee <florian.gransee@ufz.de>
2026-01-29 10:33:32 +0100

    Merge branch 'minio-parser-uuid-only-on-latest-version' into 'main'
    Minio parser uuid only on latest version
    
    See merge request ufz-tsm/tsm-orchestration!558

--- src/run_assign_parser_uuids.py
+++ src/run_assign_parser_uuids.py
@@ -13,8 +13,6 @@ logging.basicConfig(
     level=logging.INFO, format="%(asctime)s [%(levelname)s] %(message)s"
 )
 
-# MR_DATETIME = datetime.datetime(2025, 10, 28, 0, 0, 0, tzinfo=datetime.timezone.utc)
-
 
 @click.command()
 @click.option("--bucket-name")
@@ -22,7 +20,7 @@ logging.basicConfig(
 @click.option("--minio-host", default="localhost:9000")
 @click.option("--minio-user", default="minioadmin")
 @click.option("--minio-password", default="minioadmin")
-@click.option("--minio-secure", default=False)
+@click.option("--minio-secure", default=True)
 def main(bucket_name, dsn, minio_host, minio_user, minio_password, minio_secure):
     minio = Minio(
         endpoint=minio_host,
@@ -30,6 +28,7 @@ def main(bucket_name, dsn, minio_host, minio_user, minio_password, minio_secure)
         secret_key=minio_password,
         secure=minio_secure,
     )
+    n = 0
     logging.info(f"Processing bucket {bucket_name}")
     try:
         thing = Thing.from_s3_bucket_name(bucket_name, dsn=dsn)
@@ -40,20 +39,20 @@ def main(bucket_name, dsn, minio_host, minio_user, minio_password, minio_secure)
     for v in minio.list_objects(
         bucket_name,
         recursive=True,
-        include_version=True,
     ):
         if v.is_delete_marker:
+            n += 1
             continue
         if not fnmatch.fnmatch(v.object_name, filename_pattern):
+            n += 1
             continue
         try:
-            tags = minio.get_object_tags(
-                bucket_name, v.object_name, version_id=v.version_id
-            )
+            tags = minio.get_object_tags(bucket_name, v.object_name)
         except S3Error as e:
             logging.warning(
-                f"Failed to get tags for {bucket_name}/{v.object_name} (version {v.version_id}): {e}"
+                f"Failed to get tags for {bucket_name}/{v.object_name}: {e}"
             )
+            n += 1
             continue
 
         try:
@@ -65,22 +64,23 @@ def main(bucket_name, dsn, minio_host, minio_user, minio_password, minio_secure)
                 elif tags["parser_id"] == "None":
                     parser = thing.s3_store.file_parser
                 else:  # we already have a uuid
+                    logging.info("Tag 'parser_id' is already a UUID")
+                    n += 1
                     continue
             else:
                 parser = thing.s3_store.file_parser
 
             tags["parser_id"] = str(parser.uuid)
-            minio.set_object_tags(
-                bucket_name, v.object_name, tags, version_id=v.version_id
-            )
+            minio.set_object_tags(bucket_name, v.object_name, tags)
 
             logging.info(
-                f"set 'parser_id' tag for {bucket_name}/{v.object_name} (version {v.version_id}) to {parser.uuid}"
+                f"set 'parser_id' tag for {bucket_name}/{v.object_name} to {parser.uuid}"
             )
+            n += 1
 
         except Exception:
             logging.exception(
-                f"Failed to update tags for {bucket_name}/{v.object_name} (version {v.version_id})"
+                f"Failed to update tags for {bucket_name}/{v.object_name}"
             )
             continue
 
