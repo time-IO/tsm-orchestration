7365c0de1bd86692bb62808fc7d32bb41f568e79
David Schaefer <david.schaefer@ufz.de>
2026-02-03 12:32:35 +0100

    added basic tests

--- tests/test_timeio/test_qc/test_qctool_saqc.py
+++ tests/test_timeio/test_qc/test_qctool_saqc.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 import pytest
-from datetime import datetime
 
+import numpy as np
 import pandas as pd
 
 from timeio.databases import Database
@@ -12,8 +12,30 @@ from timeio.qc.utils import load_data
 from timeio.qc.saqc import init_saqc, execute_qc_function
 
 
-def select_thing_by_name(things, thing_name):
-    return [t for t in things if t.name == thing_name][0]
+@pytest.fixture()
+def local_database_connection():
+    dsn = "postgresql://postgres:postgres@localhost/postgres"
+    try:
+        conn = Database(dsn).connection()
+    except ConnectionError:
+        pytest.skip("local database connection not availan")
+
+    yield conn
+    conn.close()
+
+
+@pytest.fixture()
+def test_data():
+
+    index = pd.date_range("2020-01-01", freq="D", periods=4)
+    return {
+        "T1S33": pd.DataFrame(
+            data={"data": [800, 1000, 1100, 1300], "quality": None}, index=index
+        ),
+        "T1S36": pd.DataFrame(
+            data={"data": [750, 950, 1050, 1250], "quality": None}, index=index
+        ),
+    }
 
 
 @pytest.fixture()
@@ -23,9 +45,9 @@ def qc_functions():
             "Static-T1",
             context_window=0,
             func_name="flagRange",
-            fields=[StreamInfo(key="field", value="T1S33", thing_id=1, stream_id=33)],
-            targets=[StreamInfo(key="target", value="T1S33", thing_id=1, stream_id=33)],
-            params={"min": 900, "max": 1200}
+            fields=[StreamInfo(key="field", name="T1S33", thing_id=1, stream_id=33)],
+            targets=[StreamInfo(key="target", name="T1S33", thing_id=1, stream_id=33)],
+            params={"min": 900, "max": 1200},
         ),
         QcTest(
             "Static-T2",
@@ -33,15 +55,18 @@ def qc_functions():
             func_name="flagRange",
             fields=[StreamInfo("field", "T1S36", 1, 36)],
             targets=[StreamInfo("target", "T1S36", 1, 36)],
-            params={"min": 900, "max": 1200}
+            params={"min": 900, "max": 1200},
         ),
         QcTest(
             "Static-P1",
             context_window=0,
             func_name="processGeneric",
-            fields=[StreamInfo("field", "T1S33", 1, 33), StreamInfo("field", "T1S36", 1, 36)],
-            targets=[StreamInfo("target", "T1S33", 1, 33), StreamInfo("target", "T1S36", 1, 36)],
-            params={"func": "lambda x, y: (x + y)/2"}
+            fields=[
+                StreamInfo("field", "T1S33", 1, 33),
+                StreamInfo("field", "T1S36", 1, 36),
+            ],
+            targets=[StreamInfo("target", "T1S99", None, None)],
+            params={"func": "lambda x, y: (x + y)/2"},
         ),
         QcTest(
             "Dynamic-T1",
@@ -49,16 +74,16 @@ def qc_functions():
             func_name="flagRange",
             fields=[StreamInfo("field", "T2S44", 4, 44)],
             targets=[StreamInfo("target", "T2S44", 4, 44)],
-            params={"min": 0, "max": 15}
+            params={"min": 0, "max": 15},
         ),
-        # QcTest(
-        #     "Dynamic-T2",
-        #     context_window=0,
-        #     func_name="flagUniLOF",
-        #     fields=[StreamInfo("field", "T2S46", 4, 46)],
-        #     targets=[StreamInfo("target", "T2S46", 4, 46)],
-        #     params={}
-        # ),
+        QcTest(
+            "Dynamic-T2",
+            context_window=0,
+            func_name="flagUniLOF",
+            fields=[StreamInfo("field", "T2S46", 4, 46)],
+            targets=[StreamInfo("target", "T2S46", 4, 46)],
+            params={},
+        ),
         QcTest(
             "Dynamic-P1",
             context_window=0,
@@ -81,29 +106,43 @@ def test_collect_tests(qc_functions, thing_id, expected):
     tests = get_functions_to_execute(qc_functions, thing_id)
     assert set(set([t.name for t in tests])) == set(expected)
 
-def test_data_loading(qc_functions):
+
+def test_db_data_loading(local_database_connection, qc_functions):
+    # NOTE:
+    # test only runs if "postgresql://postgres:postgres@localhost/postgres"
+    # is available
 
     fields = []
     for f in qc_functions:
         fields.extend(f.fields)
 
-    dsn = "postgresql://postgres:postgres@localhost/postgres"
-    with Database(dsn).connection() as conn:
-        data = load_data(conn, streams=fields)
+    data = load_data(local_database_connection, streams=fields)
+    assert data.keys() == {"T1S33", "T1S36", "T2S44", "T2S46", "T1S27"}
+    for df in data.values():
+        assert isinstance(df, pd.DataFrame)
+        assert set(df.columns) == {"data", "quality"}
 
 
-def test_function_execution(qc_functions):
+def test_function_execution(qc_functions, test_data):
 
-    fields = []
-    for f in qc_functions:
-        fields.extend(f.fields)
+    index = tuple(test_data.values())[0].index
+    qc = init_saqc(test_data)
 
-    dsn = "postgresql://postgres:postgres@localhost/postgres"
-    with Database(dsn).connection() as conn:
-        data = load_data(conn, streams=fields)
-
-
-    qc = init_saqc(data)
-    for func in qc_functions:
-        print(func)
+    for func in qc_functions[:2]:
         qc = execute_qc_function(qc, func)
+
+    assert qc._flags["T1S33"].equals(
+        pd.Series([255.0, -np.inf, -np.inf, 255.0], index=index)
+    )
+    assert qc.data["T1S33"].equals(pd.Series([800, 1000, 1100, 1300], index=index))
+
+    assert qc._flags["T1S36"].equals(
+        pd.Series([255.0, -np.inf, -np.inf, 255.0], index=index)
+    )
+    assert qc.data["T1S36"].equals(pd.Series([750, 950, 1050, 1250], index=index))
+
+
+@pytest.mark.xfail
+def test_generic_function_execution(qc_functions, test_data):
+    qc = init_saqc(test_data)
+    qc = execute_qc_function(qc, qc_functions[2])
