f56221b25095d1716ba3ecd186bf8ed3940c451c
Joost Hemmen <joost.hemmen@ufz.de>
2026-02-03 12:27:25 +0100

    grant project user access to mqtt_message

--- flyway/migrations/public/V2_27__mqtt_persistence.sql
+++ flyway/migrations/public/V2_27__mqtt_persistence.sql
@@ -2,7 +2,6 @@ DO $$
 DECLARE
     schema_name TEXT;
 BEGIN
-    -- Alle Schemata finden, die eine Tabelle "observation" haben
     FOR schema_name IN
         SELECT schemaname
         FROM pg_tables
@@ -13,14 +12,19 @@ BEGIN
                 id          BIGSERIAL    NOT NULL PRIMARY KEY,
                 message     TEXT         NOT NULL,
                 thing_id    BIGINT       NOT NULL,
-                CONSTRAINT %I
+                CONSTRAINT mqtt_message_thing_id_fk_thing_id
                     FOREIGN KEY (thing_id)
                     REFERENCES %I.thing(id)
                     DEFERRABLE INITIALLY DEFERRED
             )',
-            schema_name,                                          -- Tabellenname qualifizieren
-            schema_name || '_mqtt_message_thing_id_fk_thing_id',  -- Constraint-Name eindeutig machen
-            schema_name                                           -- FK-Referenz qualifizieren
+            schema_name,
+            schema_name
+        );
+
+        EXECUTE format(
+            'GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA %I TO %I',
+            schema_name,
+            schema_name
         );
     END LOOP;
 END $$;
