8e17e1f35e814adc5035d8beeaa0aec317f77657
Tobias Kuhnert <tobias.kiuhnert@ufz.de>
2026-02-04 07:04:43 +0100

    added nginx and updated accordingly

--- frontend/app/quasar.config.ts
+++ frontend/app/quasar.config.ts
@@ -106,6 +106,7 @@ export default defineConfig((ctx) => {
     devServer: {
       // https: true,
       open: true, // opens browser window automatically
+      allowedHosts: true
     },
 
     // https://v2.quasar.dev/quasar-cli-vite/quasar-config-file#framework
