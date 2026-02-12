dcf437bee2a7bf768acf0caa8d33fe3ae5c5c7bd
Joost Hemmen <joost.hemmen@ufz.de>
2026-02-12 16:32:07 +0100

    wip, remove thing_management

--- flyway/migrations/public/V2_28__data_source_management.sql
+++ flyway/migrations/public/V2_28__data_source_management.sql
@@ -1,3 +1,4 @@
+-- create data source management user and schema
 CREATE ROLE ${dsm_db_user} WITH LOGIN PASSWORD '${dsm_db_password}';
 GRANT ${dsm_db_user} TO ${flyway:user};
 CREATE SCHEMA IF NOT EXISTS dsm_db AUTHORIZATION ${dsm_db_user};
@@ -8,3 +9,13 @@ GRANT USAGE ON SCHEMA dsm_db TO ${dsm_db_user};
 GRANT ALL ON SCHEMA dsm_db TO ${dsm_db_user};
 GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA dsm_db TO ${dsm_db_user};
 GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA dsm_db TO ${dsm_db_user};
+
+-- drop thing management schema and user
+REVOKE ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA ${thing_management_db_user} FROM ${thing_management_db_user};
+REVOKE ALL PRIVILEGES ON ALL TABLES IN SCHEMA ${thing_management_db_user} FROM ${thing_management_db_user};
+REVOKE ALL ON SCHEMA ${thing_management_db_user} FROM ${thing_management_db_user};
+REVOKE USAGE ON SCHEMA ${thing_management_db_user} FROM ${thing_management_db_user};
+REVOKE CONNECT ON DATABASE ${flyway:database} FROM ${thing_management_db_user};
+DROP SCHEMA IF EXISTS ${thing_management_db_user} CASCADE;
+REVOKE ${thing_management_db_user} FROM ${flyway:user};
+DROP ROLE IF EXISTS ${thing_management_db_user};
