987f67ff1b934ff124a7f2903c7618d65ed162e3
Tobias Kuhnert <tobias.kuhnert@ufz.de>
2026-02-19 14:09:01 +0000

    Tsm 884 create ingest mqtt

--- frontend/app/src/pages/IngestNewMqtt.vue
+++ frontend/app/src/pages/IngestNewMqtt.vue
@@ -1,105 +1,212 @@
 <template>
   <q-page class="q-pa-lg">
-    <h5>New MQTT Ingest</h5>
+    <h5 class="q-mb-none">New MQTT Ingest</h5>
     <div class="row">
       <div class="col">
-        <q-btn label="back" class="q-mb-lg" icon="chevron_left" to="/ingest/new"/>
+        <q-btn label="back" class="q-mb-lg" icon="chevron_left" to="/ingest/new" />
       </div>
     </div>
-    <q-form>
-      <q-input outlined class="q-mb-md" v-model="name" label="Name"/>
-      <q-select outlined class="q-mb-md" v-model="project" :options="projectOptions" label="Permission Group *"/>
-      <q-input outlined class="q-mb-md" v-model="description" label="Description"/>
-      <q-separator class="q-my-lg"/>
-      <q-list bordered class="q-mb-md">
-        <q-expansion-item
-          label="Stuff that will be generated"
-        >
-          <q-input outlined disable class="q-mb-md q-mx-md" model-value="" label="Username"/>
-          <q-input outlined disable class="q-mb-md q-mx-md" model-value="" label="Password"/>
-        </q-expansion-item>
-      </q-list>
-      <q-input outlined class="q-mb-md" v-model="brokerUri" label="Broker URI"/>
-      <q-input outlined class="q-mb-md" v-model="topic" label="Topic"/>
-      <q-select outlined class="q-mb-md"
-                v-model="mqttParserId"
-                use-input
-                emit-value
-                map-options
-                clearable
-                :options="filteredMqttParserOptions"
-                @filter="filterMqttParser"
-                option-value="id"
-                option-label="name"
-                label="Select the parser *"
-      >
-        <template v-slot:no-option>
-          <q-item>
-            <q-item-section class="text-grey">
-              No results
-            </q-item-section>
-          </q-item>
-        </template>
-      </q-select>
 
-      <q-btn class="full-width " label="Submit" color="green"/>
+    <q-card class="q-mb-lg" flat>
+      <q-card-section>
+        <q-form @submit.prevent="save" class="q-gutter-md">
+          <!-- Name Field -->
+          <q-input
+            filled
+            class="q-mb-md"
+            v-model="formData.name"
+            label="Name *"
+            hint="Enter a descriptive name for this ingest"
+            :rules="[(val) => !!val || 'Name is required']"
+          />
 
-    </q-form>
+          <q-select
+            filled
+            v-model="formData.permission_group_id"
+            :options="permissionGroupStore.permissionGroups"
+            label="Permission Group *"
+            option-value="id"
+            option-label="name"
+            emit-value
+            map-options
+            hint="Select the project this ingest belongs to"
+            :rules="[(val) => !!val || 'Project is required']"
+          />
 
+          <!-- Description -->
+          <q-input
+            filled
+            v-model="formData.description"
+            label="Description"
+            type="textarea"
+            rows="3"
+            hint="Provide additional details about this ingest configuration"
+          />
 
+          <q-input
+            filled
+            class="q-mb-md"
+            v-model="formData.uri"
+            label="Broker URI *"
+            :rules="[(val) => !!val || 'Broker URI is required']"
+          />
+
+          <q-input
+            filled
+            class="q-mb-md"
+            v-model="formData.topic"
+            label="Topic *"
+            :rules="[(val) => !!val || 'Topic is required']"
+          />
+
+          <q-select
+            outlined
+            class="q-mb-md"
+            v-model="formData.mqtt_parser_id"
+            use-input
+            emit-value
+            map-options
+            clearable
+            :options="filteredMqttParserOptions"
+            @filter="filterMqttParser"
+            option-value="id"
+            option-label="name"
+            label="Select the parser *"
+          >
+            <template v-slot:no-option>
+              <q-item>
+                <q-item-section class="text-grey"> No results </q-item-section>
+              </q-item>
+            </template>
+          </q-select>
+
+          <!-- Action Buttons -->
+          <div class="row q-mt-lg">
+            <q-space />
+            <div class="col-6">
+              <q-btn
+                unelevated
+                color="green"
+                type="submit"
+                :loading="isLoading"
+                label="Save"
+                class="full-width"
+              />
+            </div>
+            <q-space />
+          </div>
+        </q-form>
+      </q-card-section>
+    </q-card>
   </q-page>
 </template>
 
 <script setup lang="ts">
-import {onMounted, ref} from 'vue'
-import {useQuasar} from "quasar";
-import {useMqttParserStore} from "stores/mqttParserStore";
+import { onMounted, ref } from 'vue';
+import { useQuasar } from 'quasar';
+import { useRouter } from 'vue-router';
+import { usePermissionGroupStore } from 'stores/permissionGroupStore';
+import type { IngestMqttCreate } from 'src/services/ingest_mqtt/types';
+import { useIngestMqttStore } from 'stores/ingestMqttStore';
+import { useMqttParserStore } from 'stores/mqttParserStore';
 
-const name = ref('')
-const brokerUri = ref('')
-const topic = ref('')
-const description = ref('')
-const project = ref(null)
-const mqttParserId = ref(null)
-const projectOptions = [
-  'Project 1', 'Project 2', 'Project 3'
-]
+const mqttStore = useIngestMqttStore();
+const permissionGroupStore = usePermissionGroupStore();
+const mqttParserStore = useMqttParserStore();
+const $q = useQuasar();
+const router = useRouter();
 
-const $q = useQuasar()
-const mqttParserStore = useMqttParserStore()
+const formData = ref<IngestMqttCreate>({
+  name: null,
+  permission_group_id: null,
+  description: null,
+  topic: null,
+  uri: null,
+  mqtt_parser_id: null,
+});
 
-const filteredMqttParserOptions = ref([...mqttParserStore.mqttParsers])
+const isLoading = ref(false);
+
+const filteredMqttParserOptions = ref([...mqttParserStore.mqttParsers]);
 
 onMounted(async () => {
   try {
-    await mqttParserStore.dispatchGetList()
+    await permissionGroupStore.dispatchGetList();
   } catch {
     $q.notify({
-      position: "top",
+      position: 'top',
       type: 'negative',
-      message: 'Failed to fetch parser options'
-    })
+      message: 'Failed to fetch permission groups',
+    });
   }
-})
+
+  try {
+    await mqttParserStore.dispatchGetList();
+  } catch {
+    $q.notify({
+      position: 'top',
+      type: 'negative',
+      message: 'Failed to fetch parser options',
+    });
+  }
+});
+
+async function save() {
+  const data: IngestMqttCreate = {
+    name: formData.value.name,
+    permission_group_id: formData.value.permission_group_id,
+    description: formData.value.description,
+    topic: formData.value.topic,
+    uri: formData.value.uri,
+    mqtt_parser_id: formData.value.mqtt_parser_id,
+  };
+  try {
+    isLoading.value = true;
+    const result = await mqttStore.dispatchCreate(data);
+    $q.notify({
+      position: 'top',
+      type: 'positive',
+      message: 'Saved successfully',
+    });
+
+    // Navigate to detail
+    await router.push(`/ingest/mqtt/${result.id}`);
+  } catch (error) {
+    // @ts-expect-error to avoid complicated checks just for type safety, we ignore
+    let errorCaption = error?.response?.data?.detail || '';
+
+    // if it is a validation error, then error.response.data.detail is an array of objects [{type:string, loc: string[], msg: string, input: any, probably an object}]
+    if (typeof errorCaption === 'object') {
+      errorCaption = errorCaption[0].msg;
+    }
+
+    $q.notify({
+      position: 'top',
+      type: 'negative',
+      progress: true,
+      message: 'Failed to create ingest',
+      caption: errorCaption,
+    });
+  } finally {
+    isLoading.value = false;
+  }
+}
 
 function filterMqttParser(val: string, update: (callback: () => void) => void) {
   if (val === '') {
     update(() => {
-      filteredMqttParserOptions.value = [...mqttParserStore.mqttParsers]
-    })
-    return
+      filteredMqttParserOptions.value = [...mqttParserStore.mqttParsers];
+    });
+    return;
   }
 
   update(() => {
-    const needle = val.toLowerCase()
-    filteredMqttParserOptions.value = mqttParserStore.mqttParsers.filter(v =>
-      v.name.toLowerCase().includes(needle)
-    )
-  })
+    const needle = val.toLowerCase();
+    filteredMqttParserOptions.value = mqttParserStore.mqttParsers.filter((v) =>
+      v.name.toLowerCase().includes(needle),
+    );
+  });
 }
-
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
---
+++ frontend/app/src/stores/ingestMqttStore.ts
@@ -0,0 +1,39 @@
+import {defineStore, acceptHMRUpdate} from 'pinia';
+import type {IngestMqttCreate, IngestMqttPublic, IngestMqttUpdate} from "src/services/ingest_mqtt/types";
+import {API} from "src/services";
+
+export const useIngestMqttStore = defineStore('ingestMqttStore', {
+
+  state: () => ({
+    ingestMqttList: [] as IngestMqttPublic[],
+  }),
+
+  getters: {},
+
+  actions: {
+    async dispatchGetList() {
+      const response = await API.ingestMqtt.getList()
+      this.ingestMqttList = response.data
+    },
+    async dispatchGetOne(id: number): Promise<IngestMqttPublic> {
+      const response = await API.ingestMqtt.getOne(id)
+      return response.data
+
+    },
+    async dispatchCreate(payload: IngestMqttCreate): Promise<IngestMqttPublic> {
+      const response = await API.ingestMqtt.create(payload)
+      return response.data
+    },
+    async dispatchUpdate(id: number, payload: IngestMqttUpdate): Promise<IngestMqttPublic> {
+      const response = await API.ingestMqtt.update(id, payload)
+        return response.data
+    },
+    async dispatchDelete(id: number): Promise<void> {
+      await API.ingestMqtt.deleteOne(id)
+    },
+  },
+});
+
+if (import.meta.hot) {
+  import.meta.hot.accept(acceptHMRUpdate(useIngestMqttStore, import.meta.hot));
+}
