80b5f778156f7c1560e299a0b537a28ac9882d12
Joost Hemmen <joost.hemmen@ufz.de>
2026-02-20 14:54:24 +0100

    minor cleanup

--- api/app/start.sh
+++ api/app/start.sh
@@ -5,6 +5,5 @@ FASTAPI_ENV="${FASTAPI_ENV:-dev}"
 FASTAPI_HOST="${FASTAPI_HOST:-0.0.0.0}"
 FASTAPI_PORT="${FASTAPI_PORT:-8000}"
 
-#alembic revision --autogenerate -m "initial_setup"
 alembic upgrade head
 fastapi $FASTAPI_ENV --host $FASTAPI_HOST --port $FASTAPI_PORT main.py
