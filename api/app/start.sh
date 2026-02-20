3afad558a2430b4d9afcc9f6cb96a17c6d274756
Joost Hemmen <joost.hemmen@ufz.de>
2026-02-20 10:43:29 +0100

    initial migration and make start.sh script configurable

---
+++ api/app/start.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# set dev settings as default
+FASTAPI_ENV="${FASTAPI_ENV:-dev}"
+FASTAPI_HOST="${FASTAPI_HOST:-0.0.0.0}"
+FASTAPI_PORT="${FASTAPI_PORT:-8000}"
+
+#alembic revision --autogenerate -m "initial_setup"
+alembic upgrade head
+fastapi $FASTAPI_ENV --host $FASTAPI_HOST --port $FASTAPI_PORT main.py
