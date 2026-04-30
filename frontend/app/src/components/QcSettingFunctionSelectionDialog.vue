0b31bfbfe9fde9219e678099c6762b966cf849b3
Johanna Mardt <johanna.mardt@ufz.de>
2026-04-30 09:52:44 +0200

    q-list instead of q-card in qc setting function dialog

--- frontend/app/src/components/QcSettingFunctionSelectionDialog.vue
+++ frontend/app/src/components/QcSettingFunctionSelectionDialog.vue
@@ -1,48 +1,47 @@
 <template>
-  <q-dialog
-    v-model="showDialog"
-    backdrop-filter="blur(4px) saturate(150%)"
-    full-height
-    full-width
-    persistent
-  >
-    <q-card>
-      <q-card-section class="row">
+  <q-dialog v-model="showDialog" backdrop-filter="blur(4px) saturate(150%)" persistent>
+
+    <q-card class="column"
+  style="width: 90vw;  height: 80vh">
+      <q-card-section class="row items-center">
         <q-space />
         <div class="text-h6">Choose a quality control function</div>
         <q-space />
         <q-btn v-close-popup dense flat icon="close" round />
       </q-card-section>
-      <q-separator inset />
-      <div class="q-pa-md row items-start q-gutter-md">
-        <q-card
-          v-for="(item, i) in functionOptions"
-          @click="emitSelectFunction(item)"
-          :key="`function-option-${i}`"
-          style="width: 24%"
-          class="q-hoverable"
-        >
-          <span class="q-focus-helper"></span>
-          <q-card-section>
-            <q-item>
-              <q-item-section>
-                <span class="text-h6">{{ item.label }}</span>
-              </q-item-section>
-              <q-item-section side>
-                <q-icon
-                  name="info"
-                  class="cursor-pointer"
-                  @click.stop="openSaqcDocuForFunction(item.label)"
-                >
-                  <q-tooltip> Open SaQC Documentation </q-tooltip>
-                </q-icon>
-              </q-item-section>
-            </q-item>
-          </q-card-section>
-          <q-separator />
-          <q-card-section> {{ item.description }}</q-card-section>
-        </q-card>
-      </div>
+
+      <q-separator />
+
+      <q-scroll-area class="col">
+        <q-list separator>
+          <q-item
+            v-for="(item, i) in functionOptions"
+            :key="`function-option-${i}`"
+            clickable
+            v-ripple
+            @click="emitSelectFunction(item)"
+          >
+            <q-item-section>
+              <q-item-label class="text-weight-medium">
+                {{ item.label }}
+              </q-item-label>
+              <q-item-label caption>
+                {{ item.description }}
+              </q-item-label>
+            </q-item-section>
+
+            <q-item-section side>
+              <q-icon
+                name="info"
+                class="cursor-pointer"
+                @click.stop="openSaqcDocuForFunction(item.label)"
+              >
+                <q-tooltip> Open SaQC Documentation </q-tooltip>
+              </q-icon>
+            </q-item-section>
+          </q-item>
+        </q-list>
+      </q-scroll-area>
     </q-card>
   </q-dialog>
 </template>
@@ -53,7 +52,6 @@ import type { FunctionOption } from 'src/utils/quality_control_utils';
 const showDialog = defineModel<boolean | null>({ default: false });
 
 const emit = defineEmits(['select']);
-
 
 const functionOptions: FunctionOption[] = [
   { label: 'flagPlateau', description: 'Flag anomalous value plateaus in a time series.' },
