1aeb73619e8e03bfe4c889461fc6a33b56b308d4
granseef <florian.gransee@ufz.de>
2026-05-13 11:35:04 +0200

    add csv parser migration

--- migrations/configdb_dsm_migration/migration.py
+++ migrations/configdb_dsm_migration/migration.py
@@ -1,6 +1,6 @@
 import psycopg
 from psycopg.rows import dict_row
-from psycopg.types.json import Json
+from psycopg.types.json import Jsonb, Json
 
 from sql import queries
 
@@ -30,13 +30,59 @@ def migrate_project_and_db(cfgdb_cur, dsm_cur):
         dsm_cur.execute(queries.INSERT_DB, row)
 
 
+def migrate_parsers(cfgdb_cur, dsm_cur, django_things):
+    dsm_cur.execute("SELECT id, uuid from public.permission_group")
+    projects = dsm_cur.fetchall()
+    projects = {p["uuid"]: p["id"] for p in projects}
+    cfgdb_cur.execute(queries.GET_PARSER)
+    rows = cfgdb_cur.fetchall()
+    for row in rows:
+        if row["thing_uuid"] in django_things:
+            row["project_id"] = projects.get(row["project_uuid"])
+            params = row.get("params", {})
+            timestamp_columns = params.get("timestamp_columns")
+            row["delimiter"] = params.get("delimiter")
+            row["skiprows"] = params.get("skiprows")
+            row["skipfooter"] = params.get("skipfooter")
+            row["pandas_read_csv"] = params.get("pandas_read_csv") or {}
+            row["timezone"] = row["pandas_read_csv"].pop("timezone", None)
+            row["header"] = row["pandas_read_csv"].pop("header", None)
+            row["comment"] = Jsonb(row["pandas_read_csv"].pop("comment", None))
+            row["encoding"] = row["pandas_read_csv"].pop("encoding", None)
+            adapt_json_fields(row)
+            dsm_cur.execute(queries.INSERT_PARSER, row)
+            parser_id = dsm_cur.fetchone()["id"]
+            row["parser_id"] = parser_id
+            dsm_cur.execute(queries.INSERT_PARSER_DETAILED, row)
+            dsm_cur.execute(queries.INSERT_PARSER_CSV, row)
+            if timestamp_columns:
+                for ts in timestamp_columns:
+                    dsm_cur.execute(
+                        queries.INSERT_PARSER_TS_COLUMNS,
+                        {
+                            "parser_csv_id": parser_id,
+                            "column": ts["column"],
+                            "timestamp_format": ts["format"],
+                        },
+                    )
+
+
+def adapt_json_fields(row):
+    for k, v in row.items():
+        if isinstance(v, dict):
+            row[k] = Json(v)
+    return row
+
+
 def run_migration(cfgdb_conn, dsm_conn, django_conn):
     try:
         with dsm_conn.transaction():
             cfgdb_cur = cfgdb_conn.cursor()
             dsm_cur = dsm_conn.cursor()
             django_cur = django_conn.cursor()
+            django_things = get_django_things(django_cur)
             migrate_project_and_db(cfgdb_cur, dsm_cur)
+            migrate_parsers(cfgdb_cur, dsm_cur, django_things)
     except Exception as e:
         print(f"migration failed: {e}")
         cfgdb_conn.rollback()
--- migrations/configdb_dsm_migration/sql/queries.py
+++ migrations/configdb_dsm_migration/sql/queries.py
@@ -21,3 +21,57 @@ INSERT_DB = """
     INSERT INTO dsm_db.database (permission_group_id, "username", "password", read_only_username, read_only_password, "url", "name")
     VALUES (%(project_id)s, %(user)s, %(password)s, %(ro_user)s, %(ro_password)s, %(url)s, 'dummy')
 """
+
+GET_PARSER = """
+    SELECT fp.file_parser_type_id,
+        fp."name",
+        fp.params,
+        fp."uuid"  as "parser_uuid",
+        t.uuid   as "thing_uuid",
+        p."uuid" as "project_uuid"
+    FROM config_db.file_parser fp
+        JOIN config_db.s3_store s3 on s3.file_parser_id = fp.id
+        JOIN config_db.thing t on t.s3_store_id = s3.id
+        JOIN config_db.project p on p.id = t.project_id \
+"""
+
+INSERT_PARSER = """
+    INSERT INTO dsm_db.parser (uuid, parser_type) VALUES (%(parser_uuid)s, 'csv')
+    ON CONFLICT (uuid)
+    DO UPDATE SET
+        parser_type = EXCLUDED.parser_type
+    RETURNING id
+"""
+INSERT_PARSER_DETAILED = """
+    INSERT INTO dsm_db.parser_detailed 
+        (parser_id, permission_group_id, "name")
+        VALUES (%(parser_id)s, %(project_id)s, %(name)s)
+        ON CONFLICT (parser_id)
+        DO UPDATE SET
+            name = EXCLUDED.name
+"""
+
+
+INSERT_PARSER_CSV = """
+    INSERT INTO dsm_db.parser_csv 
+        (parser_id, delimiter, timezone, encoding, headlines_to_exclude, footlines_to_exclude, pandas_read_csv, comment, header)
+    VALUES 
+        (%(parser_id)s, %(delimiter)s, %(timezone)s, %(encoding)s, %(skiprows)s, %(skipfooter)s, %(pandas_read_csv)s, %(comment)s, %(header)s)
+    ON CONFLICT ("parser_id") 
+    DO UPDATE SET
+        delimiter = EXCLUDED.delimiter,
+        timezone = EXCLUDED.timezone,
+        encoding = EXCLUDED.encoding,
+        headlines_to_exclude = EXCLUDED.headlines_to_exclude,
+        footlines_to_exclude = EXCLUDED.footlines_to_exclude,
+        pandas_read_csv = EXCLUDED.pandas_read_csv,
+        comment = EXCLUDED.comment,
+        header = EXCLUDED.header
+"""
+
+INSERT_PARSER_TS_COLUMNS = """
+    INSERT INTO dsm_db.parser_csv_timestamp_column
+        (parser_csv_id, "column", timestamp_format)
+    VALUES
+        (%(parser_csv_id)s, %(column)s, %(timestamp_format)s)
+"""
