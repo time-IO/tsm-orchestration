22622bc946e2789c8d6b060fe323eec3382fab5d
Joost Hemmen <joost.hemmen@ufz.de>
2026-05-21 13:06:46 +0200

    set timezone

--- src/timeio/parser/mqtt_devices/quaesta.py
+++ src/timeio/parser/mqtt_devices/quaesta.py
@@ -1,24 +1,57 @@
 from __future__ import annotations
 
 from typing import Any
-from datetime import datetime
+from datetime import datetime, timezone
 
 from timeio.parser.mqtt_parser import MqttParser, Observation
 
 """
 example payload for rawdata
-{"RecordNum":1,"DateTimeUTC":"2026/03/25 13:20:52","P1_mb":984.58,"P2_mb":983.65,"T1_degC":15.6,"RH1":40.2,"Vbat":14.05,"P3_mb":983.90,"N1Cts":54,"N1ET_sec":44,"N1T(C)":11.3,"N1RH":0.2}
+ {
+  "stationID": "Site118",
+  "loggerID": "QI-DL2200-SN-25100118",
+  "type": "dataCRNS",
+  "timestampISO8601": "2026-05-20T16:00:00Z",
+  "timestampEpoch": 1779292800,
+  "dataSelect": "p3t3h3n1e1s1w1w2",
+  "recordNum": 4,
+  "P1_mb": 929.2,
+  "P2_mb": 928.11,
+  "T1_degC": 26.6,
+  "RH1_pct": 16.8,
+  "Vbat_V": 12.18,
+  "P3Extern_mb": 928.2,
+  "T3Extern_degC": 26.2,
+  "RH3Extern_pct": 18.6,
+  "N1_Cts": 2995,
+  "N1ET_sec": 3600,
+  "N1T_degC": 32.9,
+  "N1RH_pct": 16.4,
+  "SMT100_1_1": 12428,
+  "SMT100_1_2": 8.86,
+  "SMT100_1_3": 16.56,
+  "SMT100_1_4": 25.97,
+  "SMT100_1_5": 11.62,
+  "SMT100_2_1": 12118,
+  "SMT100_2_2": 9.77,
+  "SMT100_2_3": 18.38,
+  "SMT100_2_4": 25.86,
+  "SMT100_2_5": 11.59
+}
 RecordNum should not be stored, DateTimeUTC should be transformed into datetime.datetime
 """
 
 
 class QuaestaParser(MqttParser):
     def do_parse(self, rawdata: Any, origin: str = "", **kwargs) -> list[Observation]:
-        #
-        rawdata.pop("RecordNum")
-        timestamp = datetime.strptime(rawdata.pop("DateTimeUTC"), "%Y/%m/%d %H:%M:%S")
+        timestamp = datetime.strptime(rawdata.pop("timestampISO8601"), "%Y-%m-%dT%H:%M:%SZ")
+        timestamp = timestamp.replace(tzinfo=timezone.utc)
         out = []
+        if rawdata.get("type") != "dataCRNS":
+            return out
         for key, value in rawdata.items():
+            if key in ("stationID", "loggerID", "type", "timestampEpoch", "dataSelect", "recordNum"):
+                continue
             try:
                 out.append(
                     Observation(
