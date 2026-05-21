7dfffdb69c9ab38ac3191bf8b3a93789796c4ef4
Joost Hemmen <joost.hemmen@ufz.de>
2026-05-21 15:55:57 +0200

    adjust worker-configdb-updater to accept small deviation in mqtt message from dsm (as it is soon to be deprecated anyway

--- src/run_configdb_updater.py
+++ src/run_configdb_updater.py
@@ -100,8 +100,8 @@ def prepare_data_by_version(data: dict[str, Any]) -> dict[str, Any]:
     if data.get("mqtt", {}).get("username", None) is None:
         data["mqtt"] = None
     else:
-        # move top level mqtt_device_type to mqtt key
-        data["mqtt"]["mqtt_device_type"] = data["mqtt_device_type"]
+        if not data["mqtt"]["mqtt_device_type"]:
+            data["mqtt"]["mqtt_device_type"] = data["mqtt_device_type"]
 
     # set ext_sftp to None if keys have no relevant values
     if data.get("external_sftp", {}).get("uri", None) is None:
