bbd88ced41b40b3b19cae48c550ca6ed8e389b53
Johanna Mardt <johanna.mardt@ufz.de>
2026-05-28 14:50:21 +0200

    Nafornull

--- frontend/app/src/components/IngestOverviewExternalApiTable.vue
+++ frontend/app/src/components/IngestOverviewExternalApiTable.vue
@@ -11,9 +11,35 @@
     @request="onRequest"
     v-bind="$attrs"
   >
+
+    <template v-slot:header="props">
+      <q-tr :props="props">
+        <q-th
+          v-for="col in props.cols"
+          :key="col.name"
+          :props="props"
+          :class="col.name === 'action' ? 'text-center' : 'text-left'"
+        >
+          {{ col.label }}
+        </q-th>
+      </q-tr>
+    </template>
+
     <template v-slot:loading>
       <q-inner-loading showing color="primary" />
     </template>
+
+    <template v-slot:body="props">
+      <q-tr :props="props">
+        <q-td v-for="col in props.cols" :key="col.name" :props="props">
+          <span v-if="col.value !== null && col.value !== undefined && col.value !== ''">
+            {{ col.value }}
+          </span>
+          <span v-else class="text-grey-6">N/A</span>
+        </q-td>
+      </q-tr>
+    </template>
+
   </q-table>
 </template>
 
@@ -21,7 +47,7 @@
 import { onMounted, ref } from 'vue';
 import type { QTableRequestProp, QTableRequestPropPagination } from 'src/services/types';
 import { default_ingest_external_api_columns } from 'src/utils/pagination_utils';
-import type {QTableColumn} from 'quasar';
+import type { QTableColumn } from 'quasar';
 
 const props = defineProps({
   rows: {
@@ -34,8 +60,9 @@ const props = defineProps({
   },
   columns: {
     type: Array as () => QTableColumn[],
-    default: () => default_ingest_external_api_columns
+    default: () => default_ingest_external_api_columns,
   }
+
 });
 
 const pagination = defineModel<QTableRequestPropPagination>('pagination');
