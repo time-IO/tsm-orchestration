a09ef3ff59783ab095dde7b4ed65bf9ae3371dea
Maximilian Schaldach <maximilian.schaldach@ufz.de>
2026-08-14 15:53:31 +0200

    link to timeio src for local development

---
+++ data-source-management/api/app/timeio/__init__.py
@@ -0,0 +1,12 @@
+from pathlib import Path
+
+__path__.append(
+    str(Path(__file__).resolve().parents[4] / "src" / "timeio")
+)
+
+# This file refers to src/timeio directory for local development
+# Docker compose setup instead mounts src/timeio directory to this directory instead
+# During build process, src/timeio directory is copied here
+
+# Example usage of timeio modules in API:
+# from timeio.parser import get_parser
