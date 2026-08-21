c4014ec7f1afdc92af684019bad45d21a15d2e58
Tobias Kuhnert <tobias.kiuhnert@ufz.de>
2026-08-21 07:57:41 +0200

    updated scripts

--- data-source-management/api/run_alembic_migration.sh
+++ data-source-management/api/run_alembic_migration.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-docker compose -f "${SCRIPT_DIR}/../docker-compose.yml" run --rm --entrypoint "alembic upgrade head" api
+"${SCRIPT_DIR}/../../dc-with-dev.sh" run --rm --entrypoint "alembic upgrade head" dsm-api
