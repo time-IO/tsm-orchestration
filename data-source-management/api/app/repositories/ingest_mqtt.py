88191fa178d7d7e3249c747109743caf62a7f109
Tobias Kuhnert <tobias.kiuhnert@ufz.de>
2026-08-25 14:28:07 +0200

    removed unused methods

--- data-source-management/api/app/repositories/ingest_mqtt.py
+++ data-source-management/api/app/repositories/ingest_mqtt.py
@@ -230,15 +230,6 @@ class IngestMqttRepository:
         if existing:
             raise HTTPException(status_code=400, detail="This username already exists.")
 
-    @staticmethod
-    def check_payload_permission_group(
-        permission_group_id_to_check: int, permission_group_ids: list[int]
-    ):
-        if permission_group_id_to_check not in permission_group_ids:
-            raise HTTPException(
-                status_code=403,
-                detail=f"Permission denied: user does not belong to that permission group.",
-            )
 
     @staticmethod
     def to_flat(entity: IngestMqtt) -> IngestMqttRead:
