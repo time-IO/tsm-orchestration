88cbac166b4457767eb074519e6240d7c058d1b6
Tobias Kuhnert <tobias.kiuhnert@ufz.de>
2026-09-02 07:11:54 +0200

    black out

--- data-source-management/api/app/tests/validation/test_validators.py
+++ data-source-management/api/app/tests/validation/test_validators.py
@@ -207,9 +207,7 @@ class TestTypeValidatorValidateOffset:
         ],
     )
     def test_valid_offset_strings(self, value):
-        constraint = {
-            "regex": r"^(?!$)(?:\d+)?(?:B|D|W|M|Q|Y|h|min|s|ms|us|ns)?$"
-        }
+        constraint = {"regex": r"^(?!$)(?:\d+)?(?:B|D|W|M|Q|Y|h|min|s|ms|us|ns)?$"}
         assert TypeValidator.validate_offset(value, constraint) is True
 
     def test_invalid_offset_type(self):
