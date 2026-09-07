905c90fd2923c17cfa59d49539b06c939c0df314
Maximilian Schaldach <maximilian.schaldach@ufz.de>
2026-09-07 14:24:56 +0200

    add base wrapper for QField

---
+++ data-source-management/frontend/app/src/components/common/BaseField.vue
@@ -0,0 +1,27 @@
+<template>
+  <QField ref="qField" v-bind="$attrs">
+    <template v-for="(_, slot) of $slots" #[slot]="scope">
+      <slot :name="slot" v-bind="scope || {}"/>
+    </template>
+  </QField>
+</template>
+
+<script setup lang="ts">
+import {QField} from 'quasar';
+import {onMounted, ref} from 'vue';
+
+const emit = defineEmits(['click']);
+const qField = ref<QField>();
+
+const {disable} = defineProps<{
+  disable: boolean
+}>()
+onMounted(() => {
+  // https://github.com/quasarframework/quasar/issues/8956
+  qField.value!.$el.onclick = () => {
+    if (!disable) {
+      emit('click')
+    }
+  };
+});
+</script>
