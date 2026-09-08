0251260c5c0e04435786d2d93bc2c890a836f487
Maximilian Schaldach <maximilian.schaldach@ufz.de>
2026-09-08 11:06:56 +0200

    Apply 1 suggestion(s) to 1 file(s)

--- data-source-management/frontend/app/src/components/ParserSelectByType.vue
+++ data-source-management/frontend/app/src/components/ParserSelectByType.vue
@@ -10,7 +10,6 @@
   >
     <template #hint v-if="disable">
       <span>Select a Permission Group first</span>
-      <!-- TODO: move this to form components or do we always want this text? -->
     </template>
 
     <template #control>
