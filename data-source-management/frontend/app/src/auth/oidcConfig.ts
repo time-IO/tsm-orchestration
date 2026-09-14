29a54b1e76913482747fb23eeb832e26073568ba
Tobias Kuhnert <tobias.kiuhnert@ufz.de>
2026-09-14 12:47:25 +0200

    removed non-null assertion modifiers from environment variables

--- data-source-management/frontend/app/src/auth/oidcConfig.ts
+++ data-source-management/frontend/app/src/auth/oidcConfig.ts
@@ -1,12 +1,12 @@
 import { UserManager, WebStorageStateStore } from 'oidc-client-ts';
 
 const oidcConfig = {
-  authority: import.meta.env.OIDC_IDP_URL!,
-  client_id: import.meta.env.OIDC_CLIENT_ID!,
-  redirect_uri: import.meta.env.OIDC_REDIRECT_URI!,
+  authority: import.meta.env.OIDC_IDP_URL,
+  client_id: import.meta.env.OIDC_CLIENT_ID,
+  redirect_uri: import.meta.env.OIDC_REDIRECT_URI,
   response_type: 'code',
-  scope: import.meta.env.OIDC_SCOPE!,
-  post_logout_redirect_uri: import.meta.env.OIDC_POST_LOGOUT_REDIRECT_URI!,
+  scope: import.meta.env.OIDC_SCOPE,
+  post_logout_redirect_uri: import.meta.env.OIDC_POST_LOGOUT_REDIRECT_URI,
   automaticSilentRenew: true,
   includeIdTokenInSilentRenew: true,
   userStore: new WebStorageStateStore({ store: window.localStorage }),
