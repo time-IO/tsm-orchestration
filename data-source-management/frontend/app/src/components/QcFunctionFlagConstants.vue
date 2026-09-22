ad0a104374af9f5d52f113d692a87aa552dd285c
Johanna Mardt <johanna.mardt@ufz.de>
2026-09-22 10:08:17 +0200

    update after review, array: arguments optional

--- data-source-management/frontend/app/src/components/QcFunctionFlagConstants.vue
+++ data-source-management/frontend/app/src/components/QcFunctionFlagConstants.vue
@@ -175,13 +175,15 @@ const formDataWithTypes = computed(() => {
   // include required fields
   const returnArray: Array<QualityControlFunctionArgumentBase> = [
     fieldObject,
-    targetObject,
     windowObject,
     threshObject,
-    min_periodsObject,
     flagObject,
     dfilterObject,
   ];
+
+    // only add optional fields if their value is not null
+  if (targetObject.input.value !== null) returnArray.push(targetObject);
+  if (min_periodsObject.input.value !== null) returnArray.push(min_periodsObject);
 
   return returnArray;
 });
