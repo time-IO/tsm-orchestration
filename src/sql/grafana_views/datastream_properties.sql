13b3491f2cc0dc82b37dbea8064c3608ce4ce466
Joost Hemmen <joost.hemmen@ufz.de>
2026-09-22 16:42:58 +0200

    Apply 1 suggestion(s) to 1 file(s)

--- src/sql/grafana_views/datastream_properties.sql
+++ src/sql/grafana_views/datastream_properties.sql
@@ -2,7 +2,7 @@ DROP VIEW IF EXISTS "datastream_properties" CASCADE;
 CREATE VIEW "datastream_properties" AS
 SELECT
     tsm_ds."position",
-    tsm_ds.id  AS "ds_id",
-    tsm_t.uuid AS "t_uuid"
+    tsm_ds.id as "ds_id",
+    tsm_t.uuid as "t_uuid"
 FROM datastream tsm_ds
 JOIN thing tsm_t ON tsm_ds.thing_id = tsm_t.id
