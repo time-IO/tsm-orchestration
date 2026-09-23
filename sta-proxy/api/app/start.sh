cae77df86696c0a15782b731927f6d08a384cc2b
Maximilian Schaldach <maximilian.schaldach@ufz.de>
2026-09-23 14:00:08 +0200

    add services to docker compose setup

---
+++ sta-proxy/api/app/start.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e
+
+# set dev settings as default
+FASTAPI_ENV="${FASTAPI_ENV:-dev}"
+FASTAPI_HOST="${FASTAPI_HOST:-0.0.0.0}"
+FASTAPI_PORT="${FASTAPI_PORT:-8000}"
+LOG_LEVEL="${LOG_LEVEL:-info}"
+## convert log level to lowercase
+LOG_LEVEL="${LOG_LEVEL,,}"
+
+if [ "$FASTAPI_ENV" = "dev" ]; then
+	exec uvicorn main:app --host "$FASTAPI_HOST" --port "$FASTAPI_PORT" --reload --log-level "$LOG_LEVEL" --log-config /app/logging.json
+else
+	exec uvicorn main:app --host "$FASTAPI_HOST" --port "$FASTAPI_PORT" --log-level "$LOG_LEVEL" --log-config /app/logging.json
+fi
